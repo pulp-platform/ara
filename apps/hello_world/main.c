1cbcda364a0bdb0f96a6930fa622a013478c49ed
Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
2020-10-22 11:11:03 +0200

    [apps] Implement hello_world

---
+++ apps/hello_world/main.c
@@ -0,0 +1,28 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Author: Matheus Cavalcante, ETH Zurich
+
+#include <stdint.h>
+#include <string.h>
+
+#include "printf.h"
+
+int main() {
+  printf("Ariane says Hello!\n");
+
+  return 0;
+}
