7aa3701bfd7b2837788862bd6d3362873f5a95e9
Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
2020-10-27 14:51:00 +0100

    [rvtests] Add infrastructure to compile riscv-tests

--- apps/common/riscv_test.h
+++ apps/common/riscv_test.h
@@ -7,31 +7,95 @@
 // Begin Macro
 //-----------------------------------------------------------------------
 
-#define RVTEST_RV64U
+#define RVTEST_RV64U                                                    \
+  .macro init;                                                          \
+  .endm
 
 #define RVTEST_RV64UF                                                   \
-  .globl rvtest_init;                                                   \
-  rvtest_init:                                                          \
+  .macro init;                                                          \
   RVTEST_FP_ENABLE;                                                     \
-  ret
+  .endm
 
 #define RVTEST_RV64UV                                                   \
-  .globl rvtest_init;                                                   \
-  rvtest_init:                                                          \
-  RVTEST_VECTOR_ENABLE;
-  ret
+  .macro init;                                                          \
+  RVTEST_VECTOR_ENABLE;                                                 \
+  .endm
 
 #define RVTEST_RV64M                                                    \
-  .globl rvtest_init;                                                   \
-  rvtest_init:                                                          \
+  .macro init;                                                          \
   RVTEST_ENABLE_MACHINE;                                                \
-  ret
+  .endm
 
 #define RVTEST_RV64S                                                    \
-  .globl rvtest_init;                                                   \
-  rvtest_init:                                                          \
+  .macro init;                                                          \
   RVTEST_ENABLE_SUPERVISOR;                                             \
-  ret
+  .endm
+
+#if __riscv_xlen == 64
+# define CHECK_XLEN li a0, 1; slli a0, a0, 31; bgez a0, 1f; RVTEST_PASS; 1:
+#else
+# define CHECK_XLEN li a0, 1; slli a0, a0, 31; bltz a0, 1f; RVTEST_PASS; 1:
+#endif
+
+#define INIT_XREG                                                       \
+  li x1, 0;                                                             \
+  li x2, 0;                                                             \
+  li x3, 0;                                                             \
+  li x4, 0;                                                             \
+  li x5, 0;                                                             \
+  li x6, 0;                                                             \
+  li x7, 0;                                                             \
+  li x8, 0;                                                             \
+  li x9, 0;                                                             \
+  li x10, 0;                                                            \
+  li x11, 0;                                                            \
+  li x12, 0;                                                            \
+  li x13, 0;                                                            \
+  li x14, 0;                                                            \
+  li x15, 0;                                                            \
+  li x16, 0;                                                            \
+  li x17, 0;                                                            \
+  li x18, 0;                                                            \
+  li x19, 0;                                                            \
+  li x20, 0;                                                            \
+  li x21, 0;                                                            \
+  li x22, 0;                                                            \
+  li x23, 0;                                                            \
+  li x24, 0;                                                            \
+  li x25, 0;                                                            \
+  li x26, 0;                                                            \
+  li x27, 0;                                                            \
+  li x28, 0;                                                            \
+  li x29, 0;                                                            \
+  li x30, 0;                                                            \
+  li x31, 0;
+
+#define INIT_PMP                                                        \
+  la t0, 1f;                                                            \
+  csrw mtvec, t0;                                                       \
+  /* Set up a PMP to permit all accesses */                             \
+  li t0, (1 << (31 + (__riscv_xlen / 64) * (53 - 31))) - 1;             \
+  csrw pmpaddr0, t0;                                                    \
+  li t0, PMP_NAPOT | PMP_R | PMP_W | PMP_X;                             \
+  csrw pmpcfg0, t0;                                                     \
+  .align 2;                                                             \
+1:
+
+#define INIT_SATP                                                      \
+  la t0, 1f;                                                            \
+  csrw mtvec, t0;                                                       \
+  csrwi sptbr, 0;                                                       \
+  .align 2;                                                             \
+1:
+
+#define DELEGATE_NO_TRAPS                                               \
+  csrwi mie, 0;                                                         \
+  la t0, 1f;                                                            \
+  csrw mtvec, t0;                                                       \
+  csrwi medeleg, 0;                                                     \
+  csrwi mideleg, 0;                                                     \
+  .align 2;                                                             \
+1:
 
 #define RVTEST_ENABLE_SUPERVISOR                                        \
   li a0, MSTATUS_MPP & (MSTATUS_MPP >> 1);                              \
@@ -59,10 +123,16 @@
   csrr a0, mhartid;                                                     \
   1: bnez a0, 1b
 
+#define EXTRA_TVEC_USER
+#define EXTRA_TVEC_MACHINE
+#define EXTRA_INIT
+#define EXTRA_INIT_TIMER
+
+#define INTERRUPT_HANDLER j other_exception /* No interrupts should occur */
+
 #define RVTEST_CODE_BEGIN                                               \
-  .globl main;                                                          \
-  .align 2;                                                             \
-  main:                                                                 \
+        .globl main;                                                    \
+main:
 
 //-----------------------------------------------------------------------
 // End Macro
