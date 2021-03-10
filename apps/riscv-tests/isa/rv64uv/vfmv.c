f01881baa9a2fc8bc821c7af477b8cac33e58e43
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-03-10 17:01:46 +0100

    [rv64uv] Clean-up vfmv

--- apps/riscv-tests/isa/rv64uv/vfmv.c
+++ apps/riscv-tests/isa/rv64uv/vfmv.c
@@ -2,40 +2,47 @@
 // Solderpad Hardware License, Version 0.51, see LICENSE for details.
 // SPDX-License-Identifier: SHL-0.51
 //
-// Author: Matteo Perotti <mperotti@iis.ee.ethz.ch>
+// Author: Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
 //         Basile Bougenot <bbougenot@student.ethz.ch>
+//         Matteo Perotti <mperotti@student.ethz.ch>
 
 #include "vector_macros.h"
+#include "float_macros.h"
 
+void TEST_CASE1(void) {
+  VSET(16, e16, m1);
+  double dscalar_16;
+  //                            -0.9380
+  BOX_HALF_IN_DOUBLE(dscalar_16, 0xbb81);
+  VCLEAR(v1);
+  asm volatile("vfmv.v.f v1, %[A]" :: [A] "f" (dscalar_16));
+  //              -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380, -0.9380
+  VCMP_U16(1, v1, 0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81,  0xbb81);
 
-void TEST_CASE1() {
-  VSET(4,e32,m1);
-  FLOAD32(f10,_f(0.5).i);
-  __asm__ volatile("vfmv.v.f v1, f10");
-  VEC_CMP_F32(1,v1,_f(0.5).i,_f(0.5).i,_f(0.5).i,_f(0.5).i);
-}
+  VSET(16, e32, m1);
+  double dscalar_32;
+  //                             -0.96056187
+  BOX_FLOAT_IN_DOUBLE(dscalar_32, 0xbf75e762);
+  VCLEAR(v1);
+  asm volatile("vfmv.v.f v1, %[A]" :: [A] "f" (dscalar_32));
+  //               -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187, -0.96056187
+  VCMP_U32(2, v1,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762,  0xbf75e762);
 
-void TEST_CASE2() {
-  VSET(1,e64,m1);
-  FLOAD64(f10,_d(3.1415925).i);
-  __asm__ volatile("vfmv.s.f v1, f10");
-  VEC_CMP_F64(2,v1,_d(3.1415924999999998).i);
-}
-
-void TEST_CASE3() {
-  VSET(1,e32,m1);
-  VLOAD_F32(v1,_f(3.14159265).i);
-  __asm__ volatile("vfmv.f.s f10, v1");
-  FCMP32(3,f10,_f(3.14159265).i);
-}
-
+  VSET(16, e64, m1);
+  double dscalar_64;
+  //                               0.9108707261227378
+  BOX_DOUBLE_IN_DOUBLE(dscalar_64, 0x3fed25da5d7296fe);
+  VCLEAR(v1);
+  asm volatile("vfmv.v.f v1, %[A]" :: [A] "f" (dscalar_64));
+  //                0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378,  0.9108707261227378
+  VCMP_U64(3, v1,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe,  0x3fed25da5d7296fe);
+};
 
 int main(void){
-  INIT_CHECK();
   enable_vec();
   enable_fp();
+
   TEST_CASE1();
-  TEST_CASE2();
-  TEST_CASE3();
+
   EXIT_CHECK();
 }
