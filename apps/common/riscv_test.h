4e95679bfa0a9a79b3ae663e5ca3f67a0e7c43fc
Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
2021-03-18 09:54:19 +0100

    [apps] Format runtime headers with clang-format

--- apps/common/riscv_test.h
+++ apps/common/riscv_test.h
@@ -9,84 +9,82 @@
 
 #define RVTEST_RV64U
 
-#define RVTEST_RV64UF                                                   \
-  .globl rvtest_init;                                                   \
-  rvtest_init:                                                          \
-  RVTEST_FP_ENABLE;                                                     \
+#define RVTEST_RV64UF                                                          \
+  .globl rvtest_init;                                                          \
+  rvtest_init:                                                                 \
+  RVTEST_FP_ENABLE;                                                            \
   ret
 
-#define RVTEST_RV64UV                                                   \
-  .globl rvtest_init;                                                   \
-  rvtest_init:                                                          \
+#define RVTEST_RV64UV                                                          \
+  .globl rvtest_init;                                                          \
+  rvtest_init:                                                                 \
   RVTEST_VECTOR_ENABLE;
+ret
+
+#define RVTEST_RV64M                                                           \
+  .globl rvtest_init;                                                          \
+  rvtest_init:                                                                 \
+  RVTEST_ENABLE_MACHINE;                                                       \
   ret
 
-#define RVTEST_RV64M                                                    \
-  .globl rvtest_init;                                                   \
-  rvtest_init:                                                          \
-  RVTEST_ENABLE_MACHINE;                                                \
+#define RVTEST_RV64S                                                           \
+  .globl rvtest_init;                                                          \
+  rvtest_init:                                                                 \
+  RVTEST_ENABLE_SUPERVISOR;                                                    \
   ret
 
-#define RVTEST_RV64S                                                    \
-  .globl rvtest_init;                                                   \
-  rvtest_init:                                                          \
-  RVTEST_ENABLE_SUPERVISOR;                                             \
-  ret
+#define RVTEST_ENABLE_SUPERVISOR                                               \
+  li a0, MSTATUS_MPP &(MSTATUS_MPP >> 1);                                      \
+  csrs mstatus, a0;                                                            \
+  li a0, SIP_SSIP | SIP_STIP;                                                  \
+  csrs mideleg, a0;
 
-#define RVTEST_ENABLE_SUPERVISOR                                        \
-  li a0, MSTATUS_MPP & (MSTATUS_MPP >> 1);                              \
-  csrs mstatus, a0;                                                     \
-  li a0, SIP_SSIP | SIP_STIP;                                           \
-  csrs mideleg, a0;                                                     \
+#define RVTEST_ENABLE_MACHINE                                                  \
+  li a0, MSTATUS_MPP;                                                          \
+  csrs mstatus, a0;
 
-#define RVTEST_ENABLE_MACHINE                                           \
-  li a0, MSTATUS_MPP;                                                   \
-  csrs mstatus, a0;                                                     \
-
-#define RVTEST_FP_ENABLE                                                \
-  li a0, MSTATUS_FS & (MSTATUS_FS >> 1);                                \
-  csrs mstatus, a0;                                                     \
+#define RVTEST_FP_ENABLE                                                       \
+  li a0, MSTATUS_FS &(MSTATUS_FS >> 1);                                        \
+  csrs mstatus, a0;                                                            \
   csrwi fcsr, 0
 
-#define RVTEST_VECTOR_ENABLE                                            \
-  li a0, (MSTATUS_VS & (MSTATUS_VS >> 1)) |                             \
-         (MSTATUS_FS & (MSTATUS_FS >> 1));                              \
-  csrs mstatus, a0;                                                     \
-  csrwi fcsr, 0;                                                        \
+#define RVTEST_VECTOR_ENABLE                                                   \
+  li a0, (MSTATUS_VS & (MSTATUS_VS >> 1)) | (MSTATUS_FS & (MSTATUS_FS >> 1));  \
+  csrs mstatus, a0;                                                            \
+  csrwi fcsr, 0;                                                               \
   csrwi vcsr, 0;
 
-#define RISCV_MULTICORE_DISABLE                                         \
-  csrr a0, mhartid;                                                     \
-  1: bnez a0, 1b
+#define RISCV_MULTICORE_DISABLE                                                \
+  csrr a0, mhartid;                                                            \
+  1 : bnez a0, 1b
 
-#define RVTEST_CODE_BEGIN                                               \
-  .globl main;                                                          \
-  .align 2;                                                             \
-  main:                                                                 \
+#define RVTEST_CODE_BEGIN                                                      \
+  .globl main;                                                                 \
+  .align 2;                                                                    \
+  main:
 
 //-----------------------------------------------------------------------
 // End Macro
 //-----------------------------------------------------------------------
 
-#define RVTEST_CODE_END                                                 \
-        unimp
+#define RVTEST_CODE_END unimp
 
 //-----------------------------------------------------------------------
 // Pass/Fail Macro
 //-----------------------------------------------------------------------
 
-#define RVTEST_PASS                                                     \
-        li a0, 0;                                                       \
-        j _eoc
+#define RVTEST_PASS                                                            \
+  li a0, 0;                                                                    \
+  j _eoc
 
 #define TESTNUM gp
-#define RVTEST_FAIL                                                     \
-        fence;                                                          \
-1:      beqz TESTNUM, 1b;                                               \
-        sll TESTNUM, TESTNUM, 1;                                        \
-        or TESTNUM, TESTNUM, 1;                                         \
-        addi a0, TESTNUM, 0;                                            \
-        j _fail
+#define RVTEST_FAIL                                                            \
+  fence;                                                                       \
+  1 : beqz TESTNUM, 1b;                                                        \
+  sll TESTNUM, TESTNUM, 1;                                                     \
+  or TESTNUM, TESTNUM, 1;                                                      \
+  addi a0, TESTNUM, 0;                                                         \
+  j _fail
 
 //-----------------------------------------------------------------------
 // Data Section Macro
