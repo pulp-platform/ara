2957bc585ff1f15e1df2bbf16bc27dda40adac25
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-05-04 21:33:57 +0200

    [apps] Suppress warnings for unused arguments

--- apps/common/runtime.mk
+++ apps/common/runtime.mk
@@ -48,7 +48,7 @@ DEFINES := -DNR_LANES=$(nr_lanes)
 
 LLVM_FLAGS ?= -march=rv64gcv0p10 -mabi=$(RISCV_ABI) -menable-experimental-extensions -mno-relax -fuse-ld=lld
 
-RISCV_WARNINGS += -Wunused-variable -Wall -Wextra # -Werror
+RISCV_WARNINGS += -Wunused-variable -Wall -Wextra -Wno-unused-command-line-argument # -Werror
 RISCV_FLAGS    ?= $(LLVM_FLAGS) -mcmodel=medany -I$(CURDIR)/common -std=gnu99 -O3 -ffast-math -fno-common -fno-builtin-printf $(DEFINES) $(RISCV_WARNINGS)
 
 RISCV_CCFLAGS  ?= $(RISCV_FLAGS)
