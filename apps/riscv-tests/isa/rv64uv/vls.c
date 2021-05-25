46b7f2db62271b84b83fff42f99dcd3ae53c27a6
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-05-25 20:30:16 +0200

    [riscv-tests] Clean-up vls test

--- apps/riscv-tests/isa/rv64uv/vls.c
+++ apps/riscv-tests/isa/rv64uv/vls.c
@@ -2,99 +2,165 @@
 // Solderpad Hardware License, Version 0.51, see LICENSE for details.
 // SPDX-License-Identifier: SHL-0.51
 //
-// Author: Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
-//         Basile Bougenot <bbougenot@student.ethz.ch>
+// Author: Matteo Perotti <mperotti@iis.ee.ethz.ch>
 
 #include "vector_macros.h"
 
-
+// Positive-stride tests
 void TEST_CASE1(void) {
-  VSET(4,e8,m1);
-  volatile uint32_t STRIDE = 1;
-  volatile uint8_t INP[] = {0xff, 0x00, 0x0f, 0xf0};
-  MEMBARRIER;
-  __asm__ volatile ("vlse8.v v1, (%0), %[rs2]"::"r" (INP), [rs2] "r" (STRIDE));
-  VEC_CMP_U8(1,v1,0xff, 0x00, 0x0f,0xf0);
+  VSET(4, e8, m1);
+  volatile uint8_t INP1[] = {0x9f, 0xe4, 0x19, 0x20, 0x8f, 0x2e, 0x05, 0xe0, 0xf9, 0xaa, 0x71, 0xf0, 0xc3, 0x94, 0xbb, 0xd3};
+  uint64_t stride = 3;
+  asm volatile ("vlse8.v v1, (%0), %1" :: "r" (INP1), "r" (stride));
+  VCMP_U8(1, v1, 0x9f, 0x20, 0x05, 0xaa);
 }
 
-// void TEST_CASE2(void) {
-//   VSET(4,e8,m1);
-//   volatile int STRIDE = 1;
-//   volatile int8_t INP[] = {0xff, 0x00, 0x0f, 0xf0};
-//   VLOAD_8(v0,0x5,0x0,0x0,0x0);
-//   CLEAR(v1);
-//   __asm__ volatile ("vlse8.v v1, (%0), %1, v0.t"::"r" (INP), "r" (STRIDE));
-//   VEC_CMP_8(2,v1,0xff, 0x00, 0x0f,0x00);
-// }
+void TEST_CASE2(void) {
+  VSET(4, e16, m1);
+  volatile uint16_t INP1[] = {0x9fe4, 0x1920, 0x8f2e, 0x05e0, 0xf9aa, 0x71f0, 0xc394, 0xbbd3};
+  uint64_t stride = 4;
+  asm volatile ("vlse16.v v1, (%0), %1" :: "r" (INP1), "r" (stride));
+  VCMP_U16(2, v1, 0x9fe4, 0x8f2e, 0xf9aa, 0xc394);
+}
 
 void TEST_CASE3(void) {
-  VSET(4,e64,m1);
-  volatile uint32_t STRIDE = 8;
-  volatile uint64_t INP[] = {0xdeadbeefffffffff,0xdeadbeef00000000,0xdeadbeef0f0f0f0f,0xdeadbeeff0f0f0f0};
-  MEMBARRIER;
-  __asm__ volatile ("vlse64.v v1,(%0), %[rs2]"::"r" (INP), [rs2] "r" (STRIDE));
-  VEC_CMP_U64(3,v1,0xdeadbeefffffffff,0xdeadbeef00000000,0xdeadbeef0f0f0f0f,0xdeadbeeff0f0f0f0);
+  VSET(4, e32, m1);
+  volatile uint32_t INP1[] = {0x9fe41920, 0x8f2e05e0, 0xf9aa71f0, 0xc394bbd3, 0xa11a9384, 0xa7163840, 0x99991348, 0xa9f38cd1};
+  uint64_t stride = 8;
+  asm volatile ("vlse32.v v1, (%0), %1" :: "r" (INP1), "r" (stride));
+  VCMP_U32(3, v1, 0x9fe41920, 0xf9aa71f0, 0xa11a9384, 0x99991348);
 }
 
-// void TEST_CASE4(void) {
-//   VSET(4,e64,m1);
-//   volatile int STRIDE = 8;
-//   volatile int64_t INP[] = {0xdeadbeefffffffff,0xdeadbeef00000000,0xdeadbeef0f0f0f0f,0xdeadbeeff0f0f0f0};
-//   VLOAD_64(v0,0x5,0x0,0x0,0x0);
-//   CLEAR(v1);
-//   __asm__ volatile ("vlse64.v v1,(%0), %1, v0.t"::"r" (INP), "r" (STRIDE));
-//   VEC_CMP_64(4,v1,0xdeadbeefffffffff,0x0000000000000000,0xdeadbeef0f0f0f0f,0x0000000000000000);
-// }
+void TEST_CASE4(void) {
+  VSET(4, e64, m1);
+  volatile uint64_t INP1[] = {0x9fe419208f2e05e0, 0xf9aa71f0c394bbd3, 0xa11a9384a7163840, 0x99991348a9f38cd1};
+  uint64_t stride = 8;
+  asm volatile ("vlse64.v v1, (%0), %1" :: "r" (INP1), "r" (stride));
+  VCMP_U64(4, v1, 0x9fe419208f2e05e0, 0xf9aa71f0c394bbd3, 0xa11a9384a7163840, 0x99991348a9f38cd1);
+}
 
+// Zero-stride tests
+// The implementation must perform all the memory accesses
 void TEST_CASE5(void) {
-  VSET(3,e16,m1);
-  volatile uint32_t STRIDE = 2;
-  volatile uint16_t INP[] = {0xffff, 0x0000, 0x0f0f, 0xf0f0};
-  MEMBARRIER;
-  __asm__ volatile ("vlse16.v v1, (%0), %[rs2]"::"r" (INP), [rs2] "r" (STRIDE));
-  VEC_CMP_U16(5,v1,0xffff, 0x0000, 0x0f0f);
+  VSET(16, e8, m1);
+  volatile uint8_t INP1[] = {0x9f};
+  uint64_t stride = 0;
+  asm volatile ("vlse8.v v1, (%0), %1" :: "r" (INP1), "r" (stride));
+  VCMP_U8(5, v1, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f);
 }
 
-// void TEST_CASE6(void) {
-//   VSET(3,e16,m1);
-//   int STRIDE = -2;
-//   volatile int16_t INP[] = {0xffff, 0x0000, 0x0f0f, 0xf0f0};
-//   VLOAD_16(v0,0x5,0x0,0x0,0x0);
-//   CLEAR(v1);
-//   __asm__ volatile ("vlse16.v v1, (%0), %1, v0.t"::"r" (&INP[3]), "r" (STRIDE));
-//   VEC_CMP_16(6,v1,0xf0f0, 0x0000, 0x0000);
-// }
+// The implementation can also perform fewer accesses
+void TEST_CASE6(void) {
+  VSET(16, e8, m1);
+  volatile uint8_t INP1[] = {0x9f};
+  asm volatile ("vlse8.v v1, (%0), x0" :: "r" (INP1));
+  VCMP_U8(6, v1, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f, 0x9f);
+}
+
+// Different LMUL
 void TEST_CASE7(void) {
-  VSET(4,e32,m1);
-  volatile uint32_t STRIDE = 4;
-  volatile uint32_t INP[] = {0xffffffff, 0x00000000, 0x0f0f0f0f, 0xf0f0f0f0};
-  MEMBARRIER;
-  __asm__ volatile ( "vlse32.v v1, (%0), %[rs2]"  :: "r" (INP), [rs2] "r" (STRIDE));
-  VEC_CMP_U32(7,v1,0xffffffff,0x00000000, 0x0f0f0f0f,0xf0f0f0f0);
+  VSET(8, e64, m2);
+  volatile uint64_t INP1[] = {0x9fa831c7a11a9384};
+  asm volatile ("vlse64.v v2, (%0), x0" :: "r" (INP1));
+  VCMP_U64(7, v2, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384);
 }
 
+// Others
+// Negative-stride test
+void TEST_CASE8(void) {
+  VSET(4, e16, m1);
+  volatile uint16_t INP1[] = {0x9fe4, 0x1920, 0x8f2e, 0x05e0, 0xf9aa, 0x71f0, 0xc394, 0xbbd3};
+  uint64_t stride = -4;
+  asm volatile ("vlse16.v v1, (%0), %1" :: "r" (&INP1[7]), "r" (stride));
+  VCMP_U16(8, v1, 0xbbd3, 0x71f0, 0x05e0, 0x1920);
+}
 
-// void TEST_CASE8(void) {
-//   VSET(4,e32,m1);
-//   volatile int STRIDE = 4;
-//   volatile int32_t INP[] = {0xffffffff, 0x80000000, 0x0f0f0f0f, 0xf0f0f0f0};
-//   VLOAD_32(v0,0x5,0x0,0x0,0x0);
-//   CLEAR(v1);
-//   __asm__ volatile (" vlse32.v v1, (%0), %1, v0.t \n"   :: "r" (INP), "r" (STRIDE));
-//   VEC_CMP_32(8,v1,0xffffffff, 0x0, 0x0f0f0f0f, 0x0);
-// }
+// Stride greater than default Ara AXI width == 128-bit (4 lanes)
+void TEST_CASE9(void) {
+  VSET(2, e64, m1);
+  volatile uint64_t INP1[] = {0x99991348a9f38cd1, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384, 0x9fa831c7a11a9384, 0x01015ac1309bb678};
+  uint64_t stride = 40;
+  asm volatile ("vlse64.v v1, (%0), %1" :: "r" (INP1), "r" (stride));
+  VCMP_U64(9, v1, 0x99991348a9f38cd1, 0x01015ac1309bb678);
+}
 
+// Fill Ara internal Load Buffer
+void TEST_CASE10(void) {
+  VSET(8, e64, m1);
+  volatile uint64_t INP1[] = {0x9fe419208f2e05e0, 0xf9aa71f0c394bbd3, 0xa11a9384a7163840, 0x99991348a9f38cd1, \
+                              0x9fa831c7a11a9384, 0x3819759853987548, 0x1893179501093489, 0x81937598aa819388, \
+                              0x1874754791888188, 0x3eeeeeeee33111ae, 0x9013930148815808, 0xab8b914891484891, \
+                              0x9031850931584902, 0x3189759837598759, 0x8319599991911111, 0x8913984898951989};
+  uint64_t stride = 16;
+  asm volatile ("vlse64.v v1, (%0), %1" :: "r" (INP1), "r" (stride));
+  VCMP_U64(10, v1, 0x9fe419208f2e05e0, 0xa11a9384a7163840, 0x9fa831c7a11a9384, 0x1893179501093489, 0x1874754791888188, 0x9013930148815808, 0x9031850931584902, 0x8319599991911111);
+}
+
+// Masked stride loads
+void TEST_CASE11(void) {
+  VSET(4, e8, m1);
+  volatile uint8_t INP1[] = {0x9f, 0xe4, 0x19, 0x20, 0x8f, 0x2e, 0x05, 0xe0, 0xf9, 0xaa, 0x71, 0xf0, 0xc3, 0x94, 0xbb, 0xd3};
+  uint64_t stride = 3;
+  VLOAD_8(v0, 0xAA);
+  VCLEAR(v1);
+  asm volatile ("vlse8.v v1, (%0), %1, v0.t" :: "r" (INP1), "r" (stride));
+  VCMP_U8(11, v1, 0x00, 0x20, 0x00, 0xaa);
+}
+
+void TEST_CASE12(void) {
+  VSET(4, e16, m1);
+  volatile uint16_t INP1[] = {0x9fe4, 0x1920, 0x8f2e, 0x05e0, 0xf9aa, 0x71f0, 0xc394, 0xbbd3};
+  uint64_t stride = 4;
+  VLOAD_8(v0, 0xAA);
+  VCLEAR(v1);
+  asm volatile ("vlse16.v v1, (%0), %1, v0.t" :: "r" (INP1), "r" (stride));
+  VCMP_U16(12, v1, 0, 0x8f2e, 0, 0xc394);
+}
+
+void TEST_CASE13(void) {
+  VSET(4, e32, m1);
+  volatile uint32_t INP1[] = {0x9fe41920, 0x8f2e05e0, 0xf9aa71f0, 0xc394bbd3, 0xa11a9384, 0xa7163840, 0x99991348, 0xa9f38cd1};
+  uint64_t stride = 8;
+  VLOAD_8(v0, 0xAA);
+  VCLEAR(v1);
+  asm volatile ("vlse32.v v1, (%0), %1, v0.t" :: "r" (INP1), "r" (stride));
+  VCMP_U32(13, v1, 0, 0xf9aa71f0, 0, 0x99991348);
+}
+
+void TEST_CASE14(void) {
+  VSET(8, e64, m1);
+  volatile uint64_t INP1[] = {0x9fe419208f2e05e0, 0xf9aa71f0c394bbd3, 0xa11a9384a7163840, 0x99991348a9f38cd1, \
+                              0x9fa831c7a11a9384, 0x3819759853987548, 0x1893179501093489, 0x81937598aa819388, \
+                              0x1874754791888188, 0x3eeeeeeee33111ae, 0x9013930148815808, 0xab8b914891484891, \
+                              0x9031850931584902, 0x3189759837598759, 0x8319599991911111, 0x8913984898951989};
+  uint64_t stride = 16;
+  VLOAD_8(v0, 0xAA);
+  VCLEAR(v1);
+  asm volatile ("vlse64.v v1, (%0), %1, v0.t" :: "r" (INP1), "r" (stride));
+  VCMP_U64(14, v1, 0, 0xa11a9384a7163840, 0, 0x1893179501093489, 0, 0x9013930148815808, 0, 0x8319599991911111);
+}
 
 int main(void){
   INIT_CHECK();
   enable_vec();
+
   TEST_CASE1();
+  TEST_CASE2();
   TEST_CASE3();
+  TEST_CASE4();
+
   TEST_CASE5();
+  TEST_CASE6();
   TEST_CASE7();
-  // TEST_CASE2();
-  // TEST_CASE4();
-  // TEST_CASE6();
-  // TEST_CASE8();
+
+  TEST_CASE8();
+  TEST_CASE9();
+  TEST_CASE10();
+
+  TEST_CASE11();
+  TEST_CASE12();
+  TEST_CASE13();
+  TEST_CASE14();
+
   EXIT_CHECK();
 }
