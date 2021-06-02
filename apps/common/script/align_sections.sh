0c4cb04c1aba29d4d03ca4224c8996a5adb50844
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-06-02 21:41:32 +0200

    [apps] Lighten the linker-script mod script

--- apps/common/script/align_sections.sh
+++ apps/common/script/align_sections.sh
@@ -2,5 +2,6 @@
 
 # Takes as input the number of lanes ($1) and the linker script to process ($2)
 # Align the sections by AxiWideBeWidth
+# NB: this script modify ALL the ALIGN directives
 let ALIGNMENT=4*$1;
-sed -i "s/: ALIGN([0-9]*) {/: ALIGN($ALIGNMENT) {/g" $2
+sed -i "s/ALIGN([0-9]*)/ALIGN($ALIGNMENT)/g" $2
