6b8b11056d452f33f5edf1494d6032106735af0a
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-07-27 14:27:45 +0200

    [riscv_tests] :bug: Avoid overflow during vector final check

--- apps/riscv-tests/isa/rv64uv/vle1.c
+++ apps/riscv-tests/isa/rv64uv/vle1.c
@@ -24,15 +24,18 @@ void TEST_CASE1(void) {
 
   VSET(9, e64, m2);
   asm volatile ("vle1.v v1, (%0)" :: "r" (&ALIGNED_I8[1]));
-  VCMP_U8(1, v1, 0xd3, 0x40);
+  VCMP_U8(2, v1, 0xd3, 0x40);
 
   VSET(16, e64, m8);
   asm volatile ("vle1.v v1, (%0)" :: "r" (&ALIGNED_I8[1]));
-  VCMP_U8(1, v1, 0xd3, 0x40);
+  VCMP_U8(3, v1, 0xd3, 0x40);
 
   VSET(17, e64, m8);
   asm volatile ("vle1.v v1, (%0)" :: "r" (&ALIGNED_I8[1]));
-  VCMP_U8(1, v1, 0xd3, 0x40, 0xd1);
+  // The vector used by VCMP_U8 is actually 16 elements long
+  // Don't store more if you don't want to overflow
+  VSET(16, e64, m8);
+  VCMP_U8(4, v1, 0xd3, 0x40, 0xd1);
 }
 
 int main(void){
