6c37a065550adbf514801d4da70d2b23e4c57a97
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-08-02 16:35:23 +0200

    [apps] Add iconv2d to benchmarks

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -13,6 +13,14 @@ tmpscript=`mktemp`
 sed "s/ ?= /=/g" config/${config}.mk > $tmpscript
 source ${tmpscript}
 
+##########
+## AXPY ##
+##########
+
+############
+## MATMUL ##
+############
+
 # Measure the runtime of the following kernels
 for kernel in imatmul fmatmul; do
 
@@ -24,7 +32,7 @@ for kernel in imatmul fmatmul; do
 
         tempfile=`mktemp`
 
-        DEFINES="-DSIZE=$size -DKERNEL=$kernel" \
+        DEFINES="-DSIZE=$size -D${kernel^^}=1" \
                make -C apps/ bin/benchmarks
         make -C hardware/ simv app=benchmarks > $tempfile
 
@@ -33,3 +41,44 @@ for kernel in imatmul fmatmul; do
 
     done
 done
+
+############
+## CONV2D ##
+############
+
+# Measure the runtime of the following kernels
+for kernel in iconv2d; do
+
+    # Log the performance results
+    > ${kernel}_${nr_lanes}.benchmark
+
+    # Measure the following matrix and filter sizes
+    for msize in 4 8 16 32 64 128; do
+        for fsize in 3 5 7; do
+            tempfile=`mktemp`
+
+            # Generate the correct matrix and filter
+            ${PYTHON} apps/$kernel/script/gen_data.py > apps/benchmarks/data/data.S $msize $fsize
+
+            DEFINES="-D${kernel^^}=1" \
+                   make -C apps/ bin/benchmarks
+            make -C hardware/ simv app=benchmarks > $tempfile
+
+            # Extract the performance
+            cat $tempfile | grep "\[performance\]" | cut -d: -f2 >> ${kernel}_${nr_lanes}.benchmark
+
+        done
+    done
+done
+
+##############
+## JACOBI2D ##
+##############
+
+#############
+## DROPOUT ##
+#############
+
+##############
+## ROIALIGN ##
+##############
