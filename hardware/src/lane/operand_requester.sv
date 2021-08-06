5916844f72c4d337213fcc032083dcb33a84adbc
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-08-06 14:59:55 +0200

    [hardware] :bug: Process vl for operand reqs only for the store queue

--- hardware/src/lane/operand_requester.sv
+++ hardware/src/lane/operand_requester.sv
@@ -272,7 +272,7 @@ module operand_requester import ara_pkg::*; import rvv_pkg::*; #(
               // but the requester must refer to the old EEW (eew here)
               // This reasoning cannot be applied also to widening instructions, which modify vsew
               // treating it as the EEW of vd
-              vl  : operand_request_i[requester].conv == OpQueueConversionNone ?
+              vl  : (requester == StA) ?
                       ((operand_request_i[requester].vl <<
                       operand_request_i[requester].vtype.vsew) >>
                       operand_request_i[requester].eew) :
@@ -290,7 +290,7 @@ module operand_requester import ara_pkg::*; import rvv_pkg::*; #(
               // but the requester must refer to the old EEW (eew here)
               // This reasoning cannot be applied also to widening instructions, which modify vsew
               // treating it as the EEW of vd
-              len    : operand_request_i[requester].conv == OpQueueConversionNone ?
+              len    : (requester == StA) ?
                          ((operand_request_i[requester].vl <<
                          operand_request_i[requester].vtype.vsew) >>
                          operand_request_i[requester].eew) :
@@ -348,9 +348,11 @@ module operand_requester import ara_pkg::*; import rvv_pkg::*; #(
                 // Send a command to the operand queue
                 operand_queue_cmd_o[requester] = '{
                   eew : operand_request_i[requester].eew,
-                  vl    : ((operand_request_i[requester].vl <<
+                  vl  : (requester == StA) ?
+                            ((operand_request_i[requester].vl <<
                             operand_request_i[requester].vtype.vsew) >>
-                            operand_request_i[requester].eew),
+                            operand_request_i[requester].eew) :
+                            operand_request_i[requester].vl,
                   conv: operand_request_i[requester].conv
                 };
                 operand_queue_cmd_valid_o[requester] = 1'b1;
@@ -360,10 +362,11 @@ module operand_requester import ara_pkg::*; import rvv_pkg::*; #(
                   addr : vaddr(operand_request_i[requester].vs, NrLanes) +
                   (operand_request_i[requester].vstart >>
                     (int'(EW64) - int'(operand_request_i[requester].eew))),
-                  // The number of elements initially refers to vsew, but the requester must refer to eew
-                  len    : ((operand_request_i[requester].vl <<
+                  len    : (requester == StA) ?
+                             ((operand_request_i[requester].vl <<
                              operand_request_i[requester].vtype.vsew) >>
-                             operand_request_i[requester].eew),
+                             operand_request_i[requester].eew) :
+                             operand_request_i[requester].vl,
                   vew    : operand_request_i[requester].eew,
                   hazard : operand_request_i[requester].hazard,
                   default: '0
