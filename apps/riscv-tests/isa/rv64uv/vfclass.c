e5f670be2619f218c11ceb6f2648ef2bc22825d7
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-08-06 15:29:27 +0200

    [riscv-tests] Clean-up vfclass and related environment

--- apps/riscv-tests/isa/rv64uv/vfclass.c
+++ apps/riscv-tests/isa/rv64uv/vfclass.c
@@ -5,38 +5,86 @@
 // Author: Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
 //         Basile Bougenot <bbougenot@student.ethz.ch>
 
+#include "float_macros.h"
 #include "vector_macros.h"
 
-void TEST_CASE1() {
-  VSET(4, e64, m1);
-  VLOAD_F64(v2, _d(1).i, _d(-1).i, _d(3.14159265).i, _d(-3.14159265).i);
-  __asm__ volatile("vfclass.v v1, v2");
-  VEC_CMP_U64(1, v1, 64, 2, 64, 2);
-}
+// Test all the different output possibilities
+void TEST_CASE1(void) {
+  CLEAR_FFLAGS;
+  CHECK_FFLAGS(0);
 
-void TEST_CASE3() {
-  VSET(8, e32, m1);
-  VLOAD_F32(v2, _f(1).i, _f(-1).i, _f(3.14159265).i, _f(-3.14159265).i, _f(1).i,
-            _f(-1).i, _f(3.14159265).i, _f(-3.14159265).i);
-  __asm__ volatile("vfclass.v v1, v2");
-  VEC_CMP_U32(1, v1, 64, 2, 64, 2, 64, 2, 64, 2);
-}
+  VSET(16, e16, m1);
+  VLOAD_16(v2, mInfh, pInfh, qNaNh, sNaNh, 0x3b27, 0xc767, pZero, mZeroh,
+           0x8075, 0x00c5, mInfh, pInfh, qNaNh, sNaNh, 0x3b27, 0xb767);
+  asm volatile("vfclass.v v1, v2");
+  VCMP_U16(1, v1, CLASS_mInf, CLASS_pInf, CLASS_qNAN, CLASS_sNAN, CLASS_pNorm,
+           CLASS_mNorm, CLASS_pZero, CLASS_mZero, CLASS_mSub, CLASS_pSub,
+           CLASS_mInf, CLASS_pInf, CLASS_qNAN, CLASS_sNAN, CLASS_pNorm,
+           CLASS_mNorm);
 
-// void TEST_CASE2() {
-//   VSET(4,e64,m1);
-//   VLOAD_F64(v2,_d(1).i,_d(-1).i,_d(3.14159265).i,_d(-3.14159265).i);
-//   VLOAD_U64(v0,12,0,0,0);
-//   CLEAR(v1);
-//   __asm__ volatile("vfclass.v v1, v2, v0.t");
-//   VEC_CMP_U64(2,v1,0,0,64,2);
-// }
+  VSET(16, e32, m1);
+  VLOAD_32(v2, mInff, pInff, qNaNf, sNaNf, 0x3f738772, 0xbdef32e4, pZero,
+           mZerof, 0x80000075, 0x000000c5, mInff, pInff, qNaNf, sNaNf,
+           0x3f738772, 0xbdef32e4);
+  asm volatile("vfclass.v v1, v2");
+  VCMP_U32(2, v1, CLASS_mInf, CLASS_pInf, CLASS_qNAN, CLASS_sNAN, CLASS_pNorm,
+           CLASS_mNorm, CLASS_pZero, CLASS_mZero, CLASS_mSub, CLASS_pSub,
+           CLASS_mInf, CLASS_pInf, CLASS_qNAN, CLASS_sNAN, CLASS_pNorm,
+           CLASS_mNorm);
+
+  VSET(16, e64, m1);
+  VLOAD_64(v2, mInfd, pInfd, qNaNd, sNaNd, 0x3def3136584672de,
+           0xbdef3136584672de, pZero, mZerod, 0x8000000000000075,
+           0x0000000000000c5, mInfd, pInfd, qNaNd, sNaNd, 0x3def313584672de4,
+           0xbdef313654672de4);
+  asm volatile("vfclass.v v1, v2");
+  VCMP_U64(3, v1, CLASS_mInf, CLASS_pInf, CLASS_qNAN, CLASS_sNAN, CLASS_pNorm,
+           CLASS_mNorm, CLASS_pZero, CLASS_mZero, CLASS_mSub, CLASS_pSub,
+           CLASS_mInf, CLASS_pInf, CLASS_qNAN, CLASS_sNAN, CLASS_pNorm,
+           CLASS_mNorm);
+};
+
+// Test all the different output possibilities
+void TEST_CASE2(void) {
+  VSET(16, e16, m1);
+  VLOAD_16(v2, mInfh, pInfh, qNaNh, sNaNh, 0x3b27, 0xc767, pZero, mZeroh,
+           0x8075, 0x00c5, mInfh, pInfh, qNaNh, sNaNh, 0x3b27, 0xb767);
+  VLOAD_8(v0, 0xAA, 0xAA);
+  VCLEAR(v1);
+  asm volatile("vfclass.v v1, v2, v0.t");
+  VCMP_U16(4, v1, 0, CLASS_pInf, 0, CLASS_sNAN, 0, CLASS_mNorm, 0, CLASS_mZero,
+           0, CLASS_pSub, 0, CLASS_pInf, 0, CLASS_sNAN, 0, CLASS_mNorm);
+
+  VSET(16, e32, m1);
+  VLOAD_32(v2, mInff, pInff, qNaNf, sNaNf, 0x3f738772, 0xbdef32e4, pZero,
+           mZerof, 0x80000075, 0x000000c5, mInff, pInff, qNaNf, sNaNf,
+           0x3f738772, 0xbdef32e4);
+  VLOAD_8(v0, 0xAA, 0xAA);
+  VCLEAR(v1);
+  asm volatile("vfclass.v v1, v2, v0.t");
+  VCMP_U32(5, v1, 0, CLASS_pInf, 0, CLASS_sNAN, 0, CLASS_mNorm, 0, CLASS_mZero,
+           0, CLASS_pSub, 0, CLASS_pInf, 0, CLASS_sNAN, 0, CLASS_mNorm);
+
+  VSET(16, e64, m1);
+  VLOAD_64(v2, mInfd, pInfd, qNaNd, sNaNd, 0x3def313584672de4,
+           0xbdef313658467de4, pZero, mZerod, 0x8000000000000075,
+           0x0000000000000c5, mInfd, pInfd, qNaNd, sNaNd, 0x3def313658672de4,
+           0xbdef313654672de4);
+  VLOAD_8(v0, 0xAA, 0xAA);
+  VCLEAR(v1);
+  asm volatile("vfclass.v v1, v2, v0.t");
+  VCMP_U64(6, v1, 0, CLASS_pInf, 0, CLASS_sNAN, 0, CLASS_mNorm, 0, CLASS_mZero,
+           0, CLASS_pSub, 0, CLASS_pInf, 0, CLASS_sNAN, 0, CLASS_mNorm);
+  CHECK_FFLAGS(0);
+};
 
 int main(void) {
-  INIT_CHECK();
   enable_vec();
   enable_fp();
+
+  // No exception should be raised by vfclass.v
   TEST_CASE1();
-  TEST_CASE3();
-  // TEST_CASE2();
+  TEST_CASE2();
+
   EXIT_CHECK();
 }
