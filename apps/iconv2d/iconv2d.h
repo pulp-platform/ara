28bd81a61d9589137f38629ce25e9fa1d865bcaf
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-09-07 16:10:51 +0200

    [apps] Update iconv2d 7x7 with optimized alg

--- apps/iconv2d/iconv2d.h
+++ apps/iconv2d/iconv2d.h
@@ -21,12 +21,7 @@ void iconv2d_vec_4xC_slice_move_5x5(int64_t C, int64_t F);
 void iconv2d_vec_4xC_5x5(int64_t *o, int64_t *i, int64_t *f, int64_t C,
                          int64_t F);
 
-void iconv2d_7x7(int64_t *o, int64_t *i, int64_t *f, int64_t R, int64_t C,
+void iconv2d_7x7_opt(int64_t *o, int64_t *i, int64_t *f, int64_t R, int64_t C,
                  int64_t F);
-void iconv2d_vec_4xC_slice_init_7x7(int64_t *o, int64_t C);
-void iconv2d_vec_4xC_slice_preload_7x7(int64_t *i, int64_t C, int64_t F);
-void iconv2d_vec_4xC_slice_move_7x7(int64_t C, int64_t F);
-void iconv2d_vec_4xC_7x7(int64_t *o, int64_t *i, int64_t *f, int64_t C,
-                         int64_t F);
 
 #endif
