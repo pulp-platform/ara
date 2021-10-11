b8df330357ea2bc3e7a537c7aeefff9f9f03e655
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-10-11 14:44:07 +0200

    [scripts] Add convolution to benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Python in use
+PYTHON=python3
+
 # Include Ara's configuration
 if [ -z ${config} ]; then
     if [ -z ${ARA_CONFIGURATION} ]; then
@@ -13,6 +16,14 @@ tmpscript=`mktemp`
 sed "s/ ?= /=/g" config/${config}.mk > $tmpscript
 source ${tmpscript}
 
+##########
+## AXPY ##
+##########
+
+############
+## MATMUL ##
+############
+
 # Measure the runtime of the following kernels
 for kernel in imatmul fmatmul; do
 
@@ -24,7 +35,7 @@ for kernel in imatmul fmatmul; do
 
         tempfile=`mktemp`
 
-        DEFINES="-DSIZE=$size -DKERNEL=$kernel" \
+        DEFINES="-DSIZE=$size -D${kernel^^}=1" \
                make -C apps/ bin/benchmarks
         make -C hardware/ simv app=benchmarks > $tempfile
 
@@ -33,3 +44,47 @@ for kernel in imatmul fmatmul; do
 
     done
 done
+
+############
+## CONV2D ##
+############
+
+# Measure the runtime of the following kernels
+for kernel in iconv2d; do
+
+    # Log the performance results
+    > ${kernel}_${nr_lanes}.benchmark
+
+    # Measure the following matrix and filter sizes
+    # The input image is also padded, and the max vl is 128
+    # MAXVL_M2_64b - F_MAX + 1 = 128 - 7 + 1 = 122 is the max number of elements
+    # Actually 120, since it must be divible by 4
+    for msize in 4 8 16 32 64 112; do
+        for fsize in 3 5 7; do
+            tempfile=`mktemp`
+
+            # Generate the correct matrix and filter
+            ${PYTHON} apps/$kernel/script/gen_data.py > apps/benchmarks/data/data.S $msize $fsize
+
+            DEFINES="-D${kernel^^}=1" \
+                   make -C apps/ bin/benchmarks
+            make -C hardware/ simv app=benchmarks > $tempfile
+
+            # Extract the performance
+            cat $tempfile | grep "\[performance\]" | cut -d: -f2 >> ${kernel}_${nr_lanes}.benchmark
+
+        done
+    done
+done
+
+##############
+## JACOBI2D ##
+##############
+
+#############
+## DROPOUT ##
+#############
+
+##############
+## ROIALIGN ##
+##############
