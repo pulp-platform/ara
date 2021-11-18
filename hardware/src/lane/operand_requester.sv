6b82072aaef6cfca4c94ed4ece67df916d08a6e1
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2021-11-18 02:58:46 +0100

    [hardware] Fix rebase issues

--- hardware/src/lane/operand_requester.sv
+++ hardware/src/lane/operand_requester.sv
@@ -281,7 +281,7 @@ module operand_requester import ara_pkg::*; import rvv_pkg::*; #(
                       operand_request_i[requester].eew) :
                       operand_request_i[requester].vl,
               conv: operand_request_i[requester].conv,
-              ntr_red: operand_request_i[requester].cvt_resize
+              ntr_red: operand_request_i[requester].cvt_resize,
               target_fu: operand_request_i[requester].target_fu
             };
             operand_queue_cmd_valid_o[requester] = 1'b1;
@@ -364,7 +364,7 @@ module operand_requester import ara_pkg::*; import rvv_pkg::*; #(
                           operand_request_i[requester].eew) :
                           operand_request_i[requester].vl,
                   conv: operand_request_i[requester].conv,
-                  ntr_red : operand_request_i[requester].cvt_resize
+                  ntr_red : operand_request_i[requester].cvt_resize,
                   target_fu: operand_request_i[requester].target_fu
                 };
                 operand_queue_cmd_valid_o[requester] = 1'b1;
