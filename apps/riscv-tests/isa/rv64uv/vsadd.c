b33d934be78b6cfd59463562eca2230ae99c6dc3
hossein1387 <m.h.askari.hemmat@gmail.com>
2021-12-01 23:11:11 -0500

    :sparkles: Adding support for fixed-point vector instructions
    :sparkles: Adding support for VXSAT and VXMR CSRs

--- apps/riscv-tests/isa/rv64uv/vsadd.c
+++ apps/riscv-tests/isa/rv64uv/vsadd.c
@@ -14,6 +14,7 @@ void TEST_CASE1(void) {
   VLOAD_8(v2, -90, 2, 50, 4);
   __asm__ volatile("vsadd.vv v3, v1, v2" ::);
   VCMP_U8(1, v3, 0x80, 4, 127, 8);
+
   read_vxsat(vxsat);
   check_vxsat(1, vxsat, 1);
 }
@@ -27,6 +28,7 @@ void TEST_CASE2(void) {
   VCLEAR(v3);
   __asm__ volatile("vsadd.vv v3, v1, v2, v0.t" ::);
   VCMP_U8(2, v3, 0, 4, 0, 8);
+
   read_vxsat(vxsat);
   check_vxsat(2, vxsat, 0);
 }
@@ -37,6 +39,7 @@ void TEST_CASE3(void) {
   VLOAD_32(v1, 1, 0x7FFFFFFB, 3, 4);
   __asm__ volatile("vsadd.vi v3, v1, 5" ::);
   VCMP_U32(3, v3, 6, 0x7FFFFFFF, 8, 9);
+
   read_vxsat(vxsat);
   check_vxsat(3, vxsat, 1);
 }
@@ -50,6 +53,7 @@ void TEST_CASE4(void) {
   VCLEAR(v3);
   __asm__ volatile("vsadd.vi v3, v1, 5, v0.t" ::);
   VCMP_U32(4, v3, 0, 7, 0, 0x7FFFFFFF);
+
   read_vxsat(vxsat);
   check_vxsat(4, vxsat, 1);
 }
@@ -61,6 +65,7 @@ void TEST_CASE5(void) {
   const uint32_t scalar = 5;
   __asm__ volatile("vsadd.vx v3, v1, %[A]" ::[A] "r"(scalar));
   VCMP_U32(5, v3, 0x7FFFFFFF, 7, 8, 9);
+
   read_vxsat(vxsat);
   check_vxsat(5, vxsat, 1);
 }
@@ -75,6 +80,7 @@ void TEST_CASE6(void) {
   VCLEAR(v3);
   __asm__ volatile("vsadd.vx v3, v1, %[A], v0.t" ::[A] "r"(scalar));
   VCMP_U32(6, v3, 0, 0x7FFFFFFF, 0, 9);
+
   read_vxsat(vxsat);
   check_vxsat(6, vxsat, 1);
 }
