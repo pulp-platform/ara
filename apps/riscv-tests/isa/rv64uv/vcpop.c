9e013d49b3de335e0b2928d2455d57c76200e0de
Kilian Dasen <dasenk@student.ethz.ch>
2022-06-14 19:12:40 +0200

    created better vcpop unit test

--- apps/riscv-tests/isa/rv64uv/vcpop.c
+++ apps/riscv-tests/isa/rv64uv/vcpop.c
@@ -34,31 +34,54 @@ void TEST_CASE2(void) {
   XCMP(1, OUP[0], 9);
 }
 
-// vector that requires 2 beats in a 4-lane configuration
+// more elements, smaller elements, masked
 void TEST_CASE3(void) {
-  VSET(6, e64, m1);
-  VLOAD_32(v2, 0xf, 0x0, 0xf, 0x0, 0x3, 0x0);
-  VLOAD_32(v0, 0x11);
+  VSET(6, e8, m1);
+  VLOAD_8(v2, 0xf, 0x0, 0xf, 0x0, 0x3, 0x0);
+  VLOAD_8(v0, 0x1, 0x0, 0x0, 0x0, 0x7, 0x0);
   volatile uint32_t scalar = 1337;
-  volatile uint32_t OUP[] = {0, 0, 0, 0};
+  volatile uint32_t OUP[] = {0, 0, 0, 0, 0, 0};
   __asm__ volatile("vpopc.m %[A], v2, v0.t \n"
                    "sw %[A], (%1) \n"
                    :
                    : [A] "r"(scalar), "r"(OUP));
-  XCMP(1, OUP[0], 6);
+  XCMP(1, OUP[0], 3);
 }
 
-// TODO: more test cases
-//    - DONE: without mask
-//    - longer vectors with shorter element size
-//    - DONE: very long vector that does not fit in one beat
+// unmasked
+void TEST_CASE4(void) {
+  VSET(6, e8, m1);
+  VLOAD_8(v2, 0xf, 0x0, 0xf, 0x0, 0x3, 0x0);
+  volatile uint32_t scalar = 1337;
+  volatile uint32_t OUP[] = {0, 0, 0, 0, 0, 0};
+  __asm__ volatile("vpopc.m %[A], v2 \n"
+                   "sw %[A], (%1) \n"
+                   :
+                   : [A] "r"(scalar), "r"(OUP));
+  XCMP(1, OUP[0], 10);
+}
+
+// no active elements
+void TEST_CASE5(void) {
+  VSET(2, e32, m1);
+  VLOAD_32(v2, 0, 0);
+  volatile uint32_t scalar = 1234;
+  volatile uint32_t OUP[] = {0, 0};
+  __asm__ volatile("vpopc.m %[A], v2 \n"
+                   "sw %[A], (%1) \n"
+                   :
+                   : [A] "r"(scalar), "r"(OUP));
+  XCMP(1, OUP[0], 0);
+}
 
 int main(void) {
   INIT_CHECK();
   enable_vec();
   enable_fp();
   TEST_CASE1();
-  // TEST_CASE2();
-  // TEST_CASE3();
+  TEST_CASE2();
+  TEST_CASE3();
+  TEST_CASE4();
+  TEST_CASE5();
   EXIT_CHECK();
 }
