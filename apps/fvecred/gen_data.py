cb3313e512aedde937298ebaeee03f61c380dcf3
Xiaorui Yin <chinayinxiaorui@gmail.com>
2022-07-10 16:13:35 +0200

    Upload dotproduct

--- apps/fvecred/gen_data.py
+++ apps/fvecred/gen_data.py
@@ -3,9 +3,9 @@ import random
 fw = open("data.txt", 'w')
 
 fw.write("{")
-for i in range(1024):
+for i in range(2048):
     rand_num = random.randint(-4096, 4096)
-    if i == 1023:
+    if i == 2047:
         fw.write(str(rand_num) + "}")
     else:
         fw.write(str(rand_num) + ", ")
