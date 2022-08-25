7a0717b1a5cdd2860613ff12db8338b6ffabb826
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2022-08-26 00:13:11 +0200

    [hardware] :bug: Fix erroneous counter modification in SLDU

--- hardware/src/sldu/sldu.sv
+++ hardware/src/sldu/sldu.sv
@@ -545,19 +545,21 @@ module sldu import ara_pkg::*; import rvv_pkg::*; #(
         vinsn_queue_d.vinsn[vinsn_queue_q.accept_pnt].vtype.vsew = EW64;
 
       // Initialize counters
-      if (vinsn_queue_d.issue_cnt == '0)
+      if (vinsn_queue_d.issue_cnt == '0) begin
         issue_cnt_d  = pe_req_i.op inside {VSLIDEUP, VSLIDEDOWN}
                      ? pe_req_i.vl << int'(pe_req_i.vtype.vsew)
                      : (NrLanes * ($clog2(NrLanes) + 1)) << EW64;
-      if (vinsn_queue_d.commit_cnt == '0)
+        // Trim vector elements which are not written by the slide unit
+        if (pe_req_i.op == VSLIDEUP)
+          issue_cnt_d -= vinsn_queue_d.vinsn[vinsn_queue_q.accept_pnt].stride;
+      end
+      if (vinsn_queue_d.commit_cnt == '0) begin
         commit_cnt_d = pe_req_i.op inside {VSLIDEUP, VSLIDEDOWN}
                      ? pe_req_i.vl << int'(pe_req_i.vtype.vsew)
                      : (NrLanes * ($clog2(NrLanes) + 1)) << EW64;
-
-      // Trim vector elements which are not written by the slide unit
-      if (pe_req_i.op == VSLIDEUP) begin
-        issue_cnt_d -= vinsn_queue_d.vinsn[vinsn_queue_q.accept_pnt].stride;
-        commit_cnt_d -= vinsn_queue_d.vinsn[vinsn_queue_q.accept_pnt].stride;
+        // Trim vector elements which are not written by the slide unit
+        if (pe_req_i.op == VSLIDEUP)
+          commit_cnt_d -= vinsn_queue_d.vinsn[vinsn_queue_q.accept_pnt].stride;
       end
 
       // Bump pointers and counters of the vector instruction queue
