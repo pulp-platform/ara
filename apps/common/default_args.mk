f25b0395ddab7fd1c8728961afd9d85509ccbf8e
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2022-10-17 18:35:47 +0200

    [apps] Create default argument source of truth

---
+++ apps/common/default_args.mk
@@ -0,0 +1,14 @@
+# Default app parameters
+
+# Matrix size, filter size
+def_args_iconv2d     = "112 7"
+def_args_fconv2d     = "112 7"
+def_args_fconv3d     = "112 7"
+# Vector size
+def_args_fdotproduct = "512"
+# Matrix padded size 0, matrix padded size 1, onlyvec
+def_args_jacobi2d    = "130 130 0"
+# Vector size
+def_args_dropout     = "1024"
+# Vector size, data-type
+def_args_fft         = "64 float32"
