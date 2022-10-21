7c3ad792a97740bc2942127e32730d5913bcbade
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2022-10-21 14:11:03 +0200

    [apps] :art: clang-format pass

--- apps/common/rivec/vector_defines.h
+++ apps/common/rivec/vector_defines.h
@@ -72,7 +72,7 @@
 #define _MM_ADD_i32(op1, op2, vl) vadd_vv_i32m1(op1, op2, vl)
 
 #define _MM_SUB_i64(op1, op2, vl) vsub_vv_i64m1(op1, op2, vl)
-#define _MM_SUB_i32(op1, op2, vl)  vsub_vv_i32m1(op1, op2, vl)
+#define _MM_SUB_i32(op1, op2, vl) vsub_vv_i32m1(op1, op2, vl)
 
 #define _MM_MUL_i64(op1, op2, vl) vmul_vv_i64m1(op1, op2, vl)
 #define _MM_MUL_i32(op1, op2, vl) vmul_vv_i32m1(op1, op2, vl)
@@ -108,14 +108,14 @@
 #define _MM_MADD_f64(op1, op2, op3, vl) vfmadd_vv_f64m1(op1, op2, op3, vl)
 #define _MM_MADD_f32(op1, op2, op3, vl) vfmadd_vv_f32m1(op1, op2, op3, vl)
 
-#define _MM_VFCVT_F_X_f64(op1, vl)  vfcvt_f_x_v_f64m1(op1, vl)
-#define _MM_VFCVT_F_X_f32(op1, vl)  vfcvt_f_x_v_f32m1(op1, vl)
-#define _MM_VFCVT_X_F_i64(op1, vl)  vfcvt_x_f_v_i64m1(op1, vl)
-#define _MM_VFCVT_X_F_i32(op1, vl)  vfcvt_x_f_v_i32m1(op1, vl)
+#define _MM_VFCVT_F_X_f64(op1, vl) vfcvt_f_x_v_f64m1(op1, vl)
+#define _MM_VFCVT_F_X_f32(op1, vl) vfcvt_f_x_v_f32m1(op1, vl)
+#define _MM_VFCVT_X_F_i64(op1, vl) vfcvt_x_f_v_i64m1(op1, vl)
+#define _MM_VFCVT_X_F_i32(op1, vl) vfcvt_x_f_v_i32m1(op1, vl)
 #define _MM_VFWCVT_F_F_f64(op1, vl) vfwcvt_f_f_v_f64m2(op1, vl)
 #define _MM_VFNCVT_F_F_f32(op1, vl) vfncvt_f_f_w_f32m1(op1, vl)
 #define _MM_VFWCVT_F_X_f64(op1, vl) vfwcvt_f_x_v_f64m2(op1, vl)
-#define _MM_VFCVT_f32_i32(op1, vl)  vfcvt_f_x_v_f32m1(op1, vl)
+#define _MM_VFCVT_f32_i32(op1, vl) vfcvt_f_x_v_f32m1(op1, vl)
 
 #define _MM_VFLE_f64(op1, op2, vl) vmfle_vv_f64m1_b64(op1, op2, vl)
 #define _MM_VFLE_f32(op1, op2, vl) vmfle_vv_f32m1_b32(op1, op2, vl)
