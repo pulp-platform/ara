81055a025a69387a8709132ea8f57177515ce6aa
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2022-10-25 22:59:58 +0200

    [apps] Add roi_align to the apps

---
+++ apps/roi_align/kernel/roi_align.h
@@ -0,0 +1,75 @@
+/*
+    Original implementation taken from
+    https://github.com/longcw/RoIAlign.pytorch No license found on the website.
+    A question about the license was made here
+    https://github.com/longcw/RoIAlign.pytorch/issues/48 Following the answer to
+    this question, a correct header will be added also here
+    Adaptation by: Matteo Perotti, ETH Zurich, <mperotti@iis.ee.ethz.ch>
+*/
+
+#ifndef _ROI_ALIGN_H_
+#define _ROI_ALIGN_H_
+
+#include <math.h>
+
+#include "riscv_vector.h"
+#include "util.h"
+
+#ifndef SPIKE
+#include "printf.h"
+#else
+#include <stdio.h>
+#endif
+
+void printf_fx(float num);
+
+int64_t CropAndResizePerBox(const float *image_data, const int batch_size,
+                            const int depth, const int image_height,
+                            const int image_width,
+
+                            const float *boxes_data, const int *box_index_data,
+                            const int start_box, const int limit_box,
+
+                            float *crops_data, const int crop_height,
+                            const int crop_width,
+                            const float extrapolation_value);
+
+int64_t CropAndResizePerBox_BCHW_vec(
+    const float *image_data, const int batch_size, const int depth,
+    const int image_height, const int image_width,
+
+    const float *boxes_data, const int *box_index_data, const int start_box,
+    const int limit_box,
+
+    float *crops_data, const int crop_height, const int crop_width,
+    const float extrapolation_value);
+
+int64_t CropAndResizePerBox_BHWC_vec(
+    const float *image_data, const int batch_size, const int depth,
+    const int image_height, const int image_width,
+
+    const float *boxes_data, const int *box_index_data, const int start_box,
+    const int limit_box,
+
+    float *crops_data, const int crop_height, const int crop_width,
+    const float extrapolation_value);
+
+// Compare the vector and scalar implementation.
+// Return 0 if no error is found
+// Return -1 if we have an error on the first element
+// A positive return value indicates the index of the faulty element
+int verify_result(float *s_crops_data, float *v_crops_data, size_t size);
+
+// Normalized image
+void init_image(float *vec, size_t size);
+
+// Boxes must have meaningful coordinates
+void init_boxes(float *vec, size_t size);
+
+// Each box can belong to one of the #BATCH_SIZE images
+void init_boxes_idx(int *vec, size_t size, uint64_t batch_size);
+
+// Crops initialized to zero
+void init_crops(float *vec, size_t size);
+
+#endif
---
+++ apps/roi_align/main.c
@@ -0,0 +1,108 @@
+/*
+    Original implementation taken from
+    https://github.com/longcw/RoIAlign.pytorch No license found on the website.
+    A question about the license was made here
+    https://github.com/longcw/RoIAlign.pytorch/issues/48 Following the answer to
+    this question, a correct header will be added also here
+    Adaptation by: Matteo Perotti, ETH Zurich, <mperotti@iis.ee.ethz.ch>
+*/
+
+#include <stdint.h>
+#include <string.h>
+
+#include "runtime.h"
+
+#include "kernel/roi_align.h"
+
+#ifndef SPIKE
+#include "printf.h"
+#else
+#include <stdio.h>
+#endif
+
+#define EXTRAPOLATION_VALUE 0
+
+extern uint64_t BATCH_SIZE;
+extern uint64_t DEPTH;
+extern uint64_t IMAGE_HEIGHT;
+extern uint64_t IMAGE_WIDTH;
+extern uint64_t N_BOXES;
+extern uint64_t CROP_HEIGHT;
+extern uint64_t CROP_WIDTH;
+
+extern float image_data[];
+extern float boxes_data[];
+extern int box_index_data[];
+extern float crops_data[];
+extern float crops_data_vec[];
+
+int main() {
+  printf("\n");
+  printf("===============\n");
+  printf("=  RoI Align  =\n");
+  printf("===============\n");
+  printf("\n");
+  printf("\n");
+
+  int64_t err;
+  int64_t runtime_s, runtime_v;
+
+  /*
+    Initialize Matrices
+    printf("Initializing matrices...\n");
+    init_image(image_data, BATCH_SIZE * DEPTH * IMAGE_HEIGHT * IMAGE_WIDTH);
+    init_boxes(boxes_data, 4 * N_BOXES);
+    init_boxes_idx(box_index_data, N_BOXES, BATCH_SIZE);
+    Crops are already at zero
+    init_crops(crops_data, BATCH_SIZE * DEPTH * CROP_HEIGHT * CROP_WIDTH);
+    init_crops(crops_data_vec, BATCH_SIZE * DEPTH * CROP_HEIGHT * CROP_WIDTH);
+  */
+
+  // Parameters
+  printf("BATCH_SIZE = %ld\nDEPTH = %ld\nIMAGE_HEIGHT = %ld\nIMAGE_WIDTH = "
+         "%ld\nN_BOXES = %ld\nCROP_HEIGHT = %ld\nCROP_WIDTH = "
+         "%ld\nEXTRAPOLATION_VALUE = %ld\n",
+         BATCH_SIZE, DEPTH, IMAGE_HEIGHT, IMAGE_WIDTH, N_BOXES, CROP_HEIGHT,
+         CROP_WIDTH, EXTRAPOLATION_VALUE);
+
+  // Scalar benchmark
+  printf("Starting scalar benchmark...\n");
+  start_timer();
+  CropAndResizePerBox(image_data, BATCH_SIZE, DEPTH, IMAGE_HEIGHT, IMAGE_WIDTH,
+                      boxes_data, box_index_data, 0, N_BOXES, crops_data,
+                      CROP_HEIGHT, CROP_WIDTH, EXTRAPOLATION_VALUE);
+  stop_timer();
+  runtime_s = get_timer();
+  printf("Scalar benchmark complete.\n");
+  printf("Cycles: %d\n", runtime_s);
+
+  // Vector benchmark
+  printf("Starting vector benchmark...\n");
+  start_timer();
+  CropAndResizePerBox_BCHW_vec(image_data, BATCH_SIZE, DEPTH, IMAGE_HEIGHT,
+                               IMAGE_WIDTH, boxes_data, box_index_data, 0,
+                               N_BOXES, crops_data_vec, CROP_HEIGHT, CROP_WIDTH,
+                               EXTRAPOLATION_VALUE);
+  stop_timer();
+  runtime_v = get_timer();
+  printf("Vector benchmark complete.\n");
+
+  printf("The execution took %d cycles.\n", runtime_v);
+  printf("Vector speedup is %f times over the scalar implementation.\n",
+         (float)runtime_s / runtime_v);
+
+  // Check for errors
+  err = verify_result(crops_data, crops_data_vec,
+                      N_BOXES * DEPTH * CROP_HEIGHT * CROP_WIDTH);
+
+  if (err != 0) {
+    // Fix return code to match the index of the faulty element
+    err = (err == -1) ? 0 : err;
+    printf("Failed. Index %d: %x != %x\n", err, *((uint32_t *)&crops_data[err]),
+           *((uint32_t *)&crops_data_vec[err]));
+  } else {
+    printf("Passed.\n");
+  }
+
+  return 0;
+}
