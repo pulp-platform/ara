1b60c04fae1335dce7f9b68c224a4c137a77860d
M-Ijaz-10x <muhammad.ijaz@10xengineers.ai>
2022-10-29 00:12:33 +0500

    [apps] Add updated tests for vsmul, vssra and vssrl fixed-point instructions

--- apps/riscv-tests/isa/rv64uv/vssrl.c
+++ apps/riscv-tests/isa/rv64uv/vssrl.c
@@ -9,54 +9,54 @@
 
 void TEST_CASE1() {
   VSET(4, e8, m1);
-  VLOAD_U8(v2, 0xff, 0x00, 0xf0, 0x0f);
-  VLOAD_U8(v3, 1, 2, 3, 4);
+  VLOAD_8(v2, 0xff, 0x00, 0xf0, 0x0f);
+  VLOAD_8(v3, 1, 2, 3, 4);
   __asm__ volatile("vssrl.vv v1, v2, v3");
-  VEC_CMP_U8(1, v1, 0x80, 0, 0x1e, 0x01);
+  VCMP_U8(1, v1, 0x80, 0, 0x1e, 0x00);
 }
 
 void TEST_CASE2() {
   VSET(4, e8, m1);
-  VLOAD_U8(v2, 0xff, 0x00, 0xf0, 0x0f);
-  VLOAD_U8(v3, 1, 2, 3, 4);
-  VLOAD_U8(v0, 5, 0, 0, 0);
-  CLEAR(v1);
+  VLOAD_8(v2, 0xff, 0x00, 0xf0, 0x0f);
+  VLOAD_8(v3, 1, 2, 3, 4);
+  VLOAD_8(v0, 5, 0, 0, 0);
+  VCLEAR(v1);
   __asm__ volatile("vssrl.vv v1, v2, v3, v0.t");
-  VEC_CMP_U8(2, v1, 0x80, 0, 0x1e, 0);
+  VCMP_U8(2, v1, 0x80, 0, 0x1e, 0);
 }
 
 void TEST_CASE3() {
   VSET(4, e8, m1);
-  VLOAD_U8(v2, 0xff, 0x00, 0xf0, 0x0f);
+  VLOAD_8(v2, 0xff, 0x00, 0xf0, 0x0f);
   __asm__ volatile("vssrl.vi v1, v2, 5");
-  VEC_CMP_U8(3, v1, 8, 0, 8, 0);
+  VCMP_U8(3, v1, 7, 0, 7, 0);
 }
 
 void TEST_CASE4() {
   VSET(4, e8, m1);
-  VLOAD_U8(v2, 0xff, 0x00, 0xf0, 0x0f);
-  VLOAD_U8(v0, 5, 0, 0, 0);
-  CLEAR(v1);
+  VLOAD_8(v2, 0xff, 0x00, 0xf0, 0x0f);
+  VLOAD_8(v0, 5, 0, 0, 0);
+  VCLEAR(v1);
   __asm__ volatile("vssrl.vi v1, v2, 5, v0.t");
-  VEC_CMP_U8(4, v1, 8, 0, 8, 0);
+  VCMP_U8(4, v1, 7, 0, 7, 0);
 }
 
 void TEST_CASE5() {
   VSET(4, e8, m1);
-  VLOAD_U8(v2, 0xff, 0x00, 0xf0, 0x0f);
+  VLOAD_8(v2, 0xff, 0x00, 0xf0, 0x0f);
   uint64_t scalar = 5;
   __asm__ volatile("vssrl.vx v1, v2, %[A]" ::[A] "r"(scalar));
-  VEC_CMP_U8(5, v1, 8, 0, 8, 0);
+  VCMP_U8(5, v1, 7, 0, 7, 0);
 }
 
 void TEST_CASE6() {
   VSET(4, e8, m1);
-  VLOAD_U8(v2, 0xff, 0x00, 0xf0, 0x0f);
+  VLOAD_8(v2, 0xff, 0x00, 0xf0, 0x0f);
   uint64_t scalar = 5;
-  VLOAD_U8(v0, 5, 0, 0, 0);
-  CLEAR(v1);
+  VLOAD_8(v0, 5, 0, 0, 0);
+  VCLEAR(v1);
   __asm__ volatile("vssrl.vx v1, v2, %[A], v0.t" ::[A] "r"(scalar));
-  VEC_CMP_U8(6, v1, 8, 0, 8, 0);
+  VCMP_U8(6, v1, 7, 0, 7, 0);
 }
 
 int main(void) {
