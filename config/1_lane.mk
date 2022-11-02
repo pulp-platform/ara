e0c7f01ced05fbd9f78a64bb900296dcf76e108e
sharafat hussain <sharafat.hussain@10xengineers.ai>
2022-11-02 12:24:48 +0500

    [config] Add the 1_lane configuration

---
+++ config/1_lane.mk
@@ -0,0 +1,25 @@
+# Copyright 2020 ETH Zurich and University of Bologna.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Samuel Riedel, ETH Zurich
+#         Matheus Cavalcante, ETH Zurich
+
+# Number of vector lanes
+nr_lanes ?= 1
+
+# Length of each vector register (in bits)
+# Constraints: VLEN >= 512
+vlen ?= 4096
