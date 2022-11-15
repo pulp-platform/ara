da63dd910bbf6efb38f68be7f891982a74d5f4d0
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2022-11-15 19:09:05 +0100

    [apps] :bug: Fix section alignment in linker script

--- apps/common/script/align_sections.sh
+++ apps/common/script/align_sections.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Takes as input the number of lanes ($1) and the linker script to process ($2)
+# Takes as input the number of lanes ($1), the num of cores ($2), and the linker script to process ($3)
 # Align the sections by AxiWideBeWidth
 # NB: this script modify ALL the ALIGN directives
-let ALIGNMENT=4*$1;
-sed -i "s/ALIGNMENT/$ALIGNMENT/g" $2
+let ALIGNMENT=4*$1*$2;
+sed -i "s/ALIGNMENT/$ALIGNMENT/g" $3
