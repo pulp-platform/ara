26316a24a33dae30e81aee76f4a5b449d5c12d91
Chi Zhang <15927029768@163.com>
2022-11-16 16:34:24 +0100

    alignment fixed

--- apps/common/default_args.mk
+++ apps/common/default_args.mk
@@ -1,32 +1,32 @@
 # Default app parameters
 
 # Matrix size, filter size
-def_args_iconv2d     = "112 7"
-def_args_fconv2d     = "112 7"
-def_args_fconv3d     = "112 7"
+def_args_iconv2d            = "112 7"
+def_args_fconv2d            = "112 7"
+def_args_fconv3d            = "112 7"
 # Vector size
-def_args_fdotproduct = "512"
+def_args_fdotproduct        = "512"
 # Vector size
-def_args_dotproduct  = "512"
+def_args_dotproduct         = "512"
 # Matrix padded size 0, matrix padded size 1, onlyvec
-def_args_jacobi2d    = "130 130 0"
+def_args_jacobi2d           = "130 130 0"
 # Vector size
-def_args_dropout     = "1024"
+def_args_dropout            = "1024"
 # Vector size, data-type
-def_args_fft         = "64 float32"
+def_args_fft                = "64 float32"
 # Vector size
-def_args_dwt         = "512"
+def_args_dwt                = "512"
 # Vector size
-def_args_exp         = "512"
-def_args_cos         = "512"
-def_args_log         = "512"
+def_args_exp                = "512"
+def_args_cos                = "512"
+def_args_log                = "512"
 # Channels and Inner size
-def_args_softmax     = "3 256"
+def_args_softmax            = "3 256"
 # Number of steps and width of the vector
-def_args_pathfinder  = "1 1024 64"
+def_args_pathfinder         = "1 1024 64"
 # Batch_size, depth, height, width, n_boxes (in total), crop_h, crop_w
-def_args_roi_align   = "1 32 4 4 4 2 2"
+def_args_roi_align          = "1 32 4 4 4 2 2"
 #SpMV configuration: row, col, density
-def_args_spmv        = "128 128 0.6"
+def_args_spmv               = "128 128 0.6"
 #Conjugate gradient size and steps
 def_args_conjugate_gradient	= "128 0 0.5"
