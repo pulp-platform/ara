1ae7a8e0fa0f7b663bb28bf20439a3efc2583c05
Chi Zhang <15927029768@163.com>
2022-11-16 16:34:40 +0100

    Update default_args.mk

--- apps/common/default_args.mk
+++ apps/common/default_args.mk
@@ -29,4 +29,4 @@ def_args_roi_align          = "1 32 4 4 4 2 2"
 #SpMV configuration: row, col, density
 def_args_spmv               = "128 128 0.6"
 #Conjugate gradient size and steps
-def_args_conjugate_gradient	= "128 0 0.5"
+def_args_conjugate_gradient = "128 0 0.5"
