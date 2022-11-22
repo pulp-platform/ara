f341cc7ae4e91d8c68658f905cc2fcf7fd6319e4
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2022-11-22 16:30:08 +0100

    Revert "[hardware] Buffer more AXI requests from/to the system"
    This reverts commit 9216799a67c8f6cc785e14d9ebe20f6423aac87c.
    
    Temporary revert until we bump the `axi` dependency to have the
    axi_fifo available

--- hardware/src/ara_soc.sv
+++ hardware/src/ara_soc.sv
@@ -103,8 +103,8 @@ module ara_soc import axi_pkg::*; import ara_pkg::*; #(
   `AXI_LITE_TYPEDEF_ALL(soc_narrow_lite, axi_addr_t, axi_narrow_data_t, axi_narrow_strb_t)
 
   // Buses
-  system_req_t  system_axi_req, system_axi_req_spill;
-  system_resp_t system_axi_resp, system_axi_resp_spill;
+  system_req_t  system_axi_req;
+  system_resp_t system_axi_resp;
 
   soc_wide_req_t    [NrAXISlaves-1:0] periph_wide_axi_req;
   soc_wide_resp_t   [NrAXISlaves-1:0] periph_wide_axi_resp;
@@ -474,28 +474,8 @@ module ara_soc import axi_pkg::*; import ara_pkg::*; #(
     .scan_enable_i(1'b0             ),
     .scan_data_i  (1'b0             ),
     .scan_data_o  (/* Unconnected */),
-    .axi_req_o    (system_axi_req_spill ),
-    .axi_resp_i   (system_axi_resp_spill)
-  );
-
-  axi_fifo #(
-      .Depth       (16                   ),
-      .FallThrough (1'b1                 ),
-      .aw_chan_t   (system_aw_chan_t     ),
-      .w_chan_t    (system_w_chan_t      ),
-      .b_chan_t    (system_b_chan_t      ),
-      .ar_chan_t   (system_ar_chan_t     ),
-      .r_chan_t    (system_r_chan_t      ),
-      .axi_req_t   (system_req_t         ),
-      .axi_resp_t  (system_resp_t        )
-  ) i_axi_fall_through_fifo (
-      .clk_i       (clk_i                ),
-      .rst_ni      (rst_ni               ),
-      .test_i      (1'b0                 ),
-      .slv_req_i   (system_axi_req_spill ),
-      .slv_resp_o  (system_axi_resp_spill),
-      .mst_req_o   (system_axi_req       ),
-      .mst_resp_i  (system_axi_resp      )
+    .axi_req_o    (system_axi_req   ),
+    .axi_resp_i   (system_axi_resp  )
   );
 
   //////////////////
