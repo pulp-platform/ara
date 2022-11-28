4c837d93a4e7053c4156d778ce43e07bbe78d722
Shafiullah <shafi.ullah@10xengineers.ai>
2022-11-29 03:02:59 +0500

    Added mask for vfrsqrt7 flags

--- hardware/include/ara_pkg.sv
+++ hardware/include/ara_pkg.sv
@@ -1602,19 +1602,19 @@ localparam logic [63:0] E64_mInf  = 64'hfff0000000000000;
 
 
  typedef struct packed {
-  fpnew_pkg::status_t vfrsqrt7_ex_flag;
-  fp16_t              vfrsqrt7_e16;
-  } vfr_struct_e16;
+  fpnew_pkg::status_t ex_flag;
+  fp16_t              vf7_e16;
+  } vf7_struct_e16;
 
   typedef struct packed {
-  fpnew_pkg::status_t vfrsqrt7_ex_flag;
-  fp32_t              vfrsqrt7_e32;
-  } vfr_struct_e32;
+  fpnew_pkg::status_t ex_flag;
+  fp32_t              vf7_e32;
+  } vf7_struct_e32;
 
  typedef struct packed {
-  fpnew_pkg::status_t vfrsqrt7_ex_flag;
-  fp64_t              vfrsqrt7_e64;
-  } vfr_struct_e64;
+  fpnew_pkg::status_t ex_flag;
+  fp64_t              vf7_e64;
+  } vf7_struct_e64;
 
   function automatic logic [LUT_BITS-1:0] vfrsqrt7_lut(logic [LUT_BITS-1:0] vfrsqrt7_lut_select);
       logic [LUT_BITS-1:0] vfrsqrt7_lut_out;
@@ -1754,18 +1754,18 @@ localparam logic [63:0] E64_mInf  = 64'hfff0000000000000;
   //  VFRSQRT7 OUTPUT //
   ////////////////////
 //for SEW=16
-  function automatic vfr_struct_e16 vfrsqrt7_fp16(logic [VF_TYPE_SEL_BITS-1:0] vfpu_result, logic [E16_BITS-1:0] operand_a_delay, logic [3:0] leading_zeros_count);
-     vfr_struct_e16 vfrsqrt7_o;
+  function automatic vf7_struct_e16 vfrsqrt7_fp16(logic [VF_TYPE_SEL_BITS-1:0] vfpu_result, logic [E16_BITS-1:0] operand_a_delay, logic [3:0] leading_zeros_count);
+     vf7_struct_e16 vfrsqrt7_o;
      fp16_t         vfrsqrt7_i;
 
      logic [EXP_BITS_E16:0]  vfrsqrt7_exp_i,
                              vfrsqrt7_exp_o;
 
-     vfrsqrt7_o.vfrsqrt7_e16     = 16'd0;
-     vfrsqrt7_o.vfrsqrt7_ex_flag = '{1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
-     vfrsqrt7_i        = 16'd0;
-     vfrsqrt7_exp_o    = 6'd0;
-     vfrsqrt7_exp_i    = 6'd0;
+     vfrsqrt7_o.vf7_e16 = 16'd0;
+     vfrsqrt7_o.ex_flag = 5'b0;
+     vfrsqrt7_i         = 16'd0;
+     vfrsqrt7_exp_o     = 6'd0;
+     vfrsqrt7_exp_i     = 6'd0;
          unique case (vfpu_result[6:5])
          2'b01: begin // POSSUBNORM
                 //As input is subnormal, So
@@ -1790,20 +1790,20 @@ localparam logic [63:0] E64_mInf  = 64'hfff0000000000000;
           fpnew_pkg:: NEGINF,
           fpnew_pkg:: NEGNORM,
           fpnew_pkg:: NEGSUBNORM,
-          fpnew_pkg:: QNAN : begin
-                                 vfrsqrt7_o.vfrsqrt7_e16 = E16_NaN;
-                                 vfrsqrt7_o.vfrsqrt7_ex_flag.NV=1'b1;
+          fpnew_pkg:: SNAN : begin
+                                 vfrsqrt7_o.vf7_e16    = E16_NaN;
+                                 vfrsqrt7_o.ex_flag.NV = 1'b1;
                              end
-          fpnew_pkg:: SNAN :     vfrsqrt7_o.vfrsqrt7_e16 = E16_NaN;
+          fpnew_pkg:: QNAN :     vfrsqrt7_o.vf7_e16    = E16_NaN;
           fpnew_pkg:: NEGZERO: begin
-                                 vfrsqrt7_o.vfrsqrt7_e16 = E16_mInf;
-                                 vfrsqrt7_o.vfrsqrt7_ex_flag.DZ=1'b1;
+                                 vfrsqrt7_o.vf7_e16    = E16_mInf;
+                                 vfrsqrt7_o.ex_flag.DZ = 1'b1;
                                end
           fpnew_pkg:: POSZERO: begin
-                                 vfrsqrt7_o.vfrsqrt7_e16 = E16_pInf;
-                                 vfrsqrt7_o.vfrsqrt7_ex_flag.DZ=1'b1;
+                                 vfrsqrt7_o.vf7_e16    = E16_pInf;
+                                 vfrsqrt7_o.ex_flag.DZ = 1'b1;
                                end
-          fpnew_pkg:: POSINF:    vfrsqrt7_o.vfrsqrt7_e16 = 16'd0;
+          fpnew_pkg:: POSINF:    vfrsqrt7_o.vf7_e16    = 16'd0;
           fpnew_pkg:: POSSUBNORM,
           fpnew_pkg:: POSNORM:
                          begin
@@ -1812,28 +1812,28 @@ localparam logic [63:0] E64_mInf  = 64'hfff0000000000000;
                                 //      = (3*B+(~exp_i))/2
                                 vfrsqrt7_exp_o = E16_3xB +(~vfrsqrt7_exp_i);
                                      // dividing by 2
-                                vfrsqrt7_o.vfrsqrt7_e16.e = vfrsqrt7_exp_o[5:1];
+                                vfrsqrt7_o.vf7_e16.e = vfrsqrt7_exp_o[5:1];
                                 //Output significand(mantissa) can be found by
                                 //concatenating LSB of the normalized input exponent and
                                 //the six MSBs of the normalized input significand
-                                vfrsqrt7_o.vfrsqrt7_e16.m[9:3] = vfrsqrt7_lut({vfrsqrt7_exp_i[0],vfrsqrt7_i.m[9:4]});
+                                vfrsqrt7_o.vf7_e16.m[9:3] = vfrsqrt7_lut({vfrsqrt7_exp_i[0],vfrsqrt7_i.m[9:4]});
                                  //The output sign equals the input sign.
-                                vfrsqrt7_o.vfrsqrt7_e16.s = vfrsqrt7_i.s;
+                                vfrsqrt7_o.vf7_e16.s = vfrsqrt7_i.s;
                          end
          endcase
       return vfrsqrt7_o;
   endfunction : vfrsqrt7_fp16
 
 //for SEW=32
-  function automatic vfr_struct_e32 vfrsqrt7_fp32(logic [VF_TYPE_SEL_BITS-1:0] vfpu_result, logic [E32_BITS-1:0] operand_a_delay, logic [4:0] leading_zeros_count);
-     vfr_struct_e32 vfrsqrt7_o;
+  function automatic vf7_struct_e32 vfrsqrt7_fp32(logic [VF_TYPE_SEL_BITS-1:0] vfpu_result, logic [E32_BITS-1:0] operand_a_delay, logic [4:0] leading_zeros_count);
+     vf7_struct_e32 vfrsqrt7_o;
      fp32_t         vfrsqrt7_i;
 
      logic [EXP_BITS_E32:0]  vfrsqrt7_exp_i,
                              vfrsqrt7_exp_o;
 
-     vfrsqrt7_o.vfrsqrt7_e32     = 32'd0;
-     vfrsqrt7_o.vfrsqrt7_ex_flag = '{1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
+     vfrsqrt7_o.vf7_e32     = 32'd0;
+     vfrsqrt7_o.ex_flag = 5'b0;
 
      vfrsqrt7_i        = 32'd0;
      vfrsqrt7_exp_o    = 9'd0;
@@ -1862,20 +1862,20 @@ localparam logic [63:0] E64_mInf  = 64'hfff0000000000000;
           fpnew_pkg:: NEGINF,
           fpnew_pkg:: NEGNORM,
           fpnew_pkg:: NEGSUBNORM,
-          fpnew_pkg:: QNAN : begin
-                                 vfrsqrt7_o.vfrsqrt7_e32 = E32_NaN;
-                                 vfrsqrt7_o.vfrsqrt7_ex_flag.NV=1'b1;
+          fpnew_pkg:: SNAN : begin
+                                 vfrsqrt7_o.vf7_e32    = E32_NaN;
+                                 vfrsqrt7_o.ex_flag.NV = 1'b1;
                              end
-          fpnew_pkg:: SNAN :     vfrsqrt7_o.vfrsqrt7_e32 = E32_NaN;
+          fpnew_pkg:: QNAN :     vfrsqrt7_o.vf7_e32    = E32_NaN;
           fpnew_pkg:: NEGZERO: begin
-                                 vfrsqrt7_o.vfrsqrt7_e32 = E32_mInf;
-                                 vfrsqrt7_o.vfrsqrt7_ex_flag.DZ=1'b1;
+                                 vfrsqrt7_o.vf7_e32    = E32_mInf;
+                                 vfrsqrt7_o.ex_flag.DZ = 1'b1;
                                end
           fpnew_pkg:: POSZERO: begin
-                                 vfrsqrt7_o.vfrsqrt7_e32 = E32_pInf;
-                                 vfrsqrt7_o.vfrsqrt7_ex_flag.DZ=1'b1;
+                                 vfrsqrt7_o.vf7_e32    = E32_pInf;
+                                 vfrsqrt7_o.ex_flag.DZ = 1'b1;
                                end
-          fpnew_pkg:: POSINF:    vfrsqrt7_o.vfrsqrt7_e32 = 32'd0;
+          fpnew_pkg:: POSINF:    vfrsqrt7_o.vf7_e32     = 32'd0;
           fpnew_pkg:: POSSUBNORM,
           fpnew_pkg:: POSNORM:
                          begin
@@ -1884,13 +1884,13 @@ localparam logic [63:0] E64_mInf  = 64'hfff0000000000000;
                                 //      = (3*B+(~exp_i))/2
                                 vfrsqrt7_exp_o = E32_3xB +(~vfrsqrt7_exp_i);
                                      // dividing by 2
-                                vfrsqrt7_o.vfrsqrt7_e32.e = vfrsqrt7_exp_o[8:1];
+                                vfrsqrt7_o.vf7_e32.e = vfrsqrt7_exp_o[8:1];
                                 //Output significand(mantissa) can be found by
                                 //concatenating LSB of the normalized input exponent and
                                 //the six MSBs of the normalized input significand
-                                vfrsqrt7_o.vfrsqrt7_e32.m[22:16] = vfrsqrt7_lut({vfrsqrt7_exp_i [0],vfrsqrt7_i.m[22:17]});
+                                vfrsqrt7_o.vf7_e32.m[22:16] = vfrsqrt7_lut({vfrsqrt7_exp_i [0],vfrsqrt7_i.m[22:17]});
                                  //The output sign equals the input sign.
-                                vfrsqrt7_o.vfrsqrt7_e32.s = vfrsqrt7_i.s;
+                                vfrsqrt7_o.vf7_e32.s = vfrsqrt7_i.s;
                          end
          endcase
       return vfrsqrt7_o;
@@ -1898,15 +1898,15 @@ localparam logic [63:0] E64_mInf  = 64'hfff0000000000000;
 
 
 //for SEW=64
-  function automatic vfr_struct_e64 vfrsqrt7_fp64(logic [VF_TYPE_SEL_BITS-1:0] vfpu_result, logic [E64_BITS-1:0] operand_a_delay, logic [5:0] leading_zeros_count);
-     vfr_struct_e64 vfrsqrt7_o;
+  function automatic vf7_struct_e64 vfrsqrt7_fp64(logic [VF_TYPE_SEL_BITS-1:0] vfpu_result, logic [E64_BITS-1:0] operand_a_delay, logic [5:0] leading_zeros_count);
+     vf7_struct_e64 vfrsqrt7_o;
      fp64_t     vfrsqrt7_i;
 
      logic [EXP_BITS_E64:0]  vfrsqrt7_exp_i,
                              vfrsqrt7_exp_o;
 
-     vfrsqrt7_o.vfrsqrt7_e64   = 64'd0;
-     vfrsqrt7_o.vfrsqrt7_ex_flag = '{1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
+     vfrsqrt7_o.vf7_e64   = 64'd0;
+     vfrsqrt7_o.ex_flag = 5'b0;
 
      vfrsqrt7_i        = 64'd0;
      vfrsqrt7_exp_o    = 12'd0;
@@ -1935,20 +1935,20 @@ localparam logic [63:0] E64_mInf  = 64'hfff0000000000000;
           fpnew_pkg:: NEGINF,
           fpnew_pkg:: NEGNORM,
           fpnew_pkg:: NEGSUBNORM,
-          fpnew_pkg:: QNAN : begin
-                                 vfrsqrt7_o.vfrsqrt7_e64        = E64_NaN;
-                                 vfrsqrt7_o.vfrsqrt7_ex_flag.NV = 1'b1;
+          fpnew_pkg:: SNAN : begin
+                                 vfrsqrt7_o.vf7_e64    = E64_NaN;
+                                 vfrsqrt7_o.ex_flag.NV = 1'b1;
                              end
-          fpnew_pkg:: SNAN :     vfrsqrt7_o.vfrsqrt7_e64        = E64_NaN;
+          fpnew_pkg:: QNAN :     vfrsqrt7_o.vf7_e64    = E64_NaN;
           fpnew_pkg:: NEGZERO: begin
-                                 vfrsqrt7_o.vfrsqrt7_e64        = E64_mInf;
-                                 vfrsqrt7_o.vfrsqrt7_ex_flag.DZ = 1'b1;
+                                 vfrsqrt7_o.vf7_e64    = E64_mInf;
+                                 vfrsqrt7_o.ex_flag.DZ = 1'b1;
                                end
           fpnew_pkg:: POSZERO: begin
-                                 vfrsqrt7_o.vfrsqrt7_e64        = E64_pInf;
-                                 vfrsqrt7_o.vfrsqrt7_ex_flag.DZ = 1'b1;
+                                 vfrsqrt7_o.vf7_e64    = E64_pInf;
+                                 vfrsqrt7_o.ex_flag.DZ = 1'b1;
                                end
-          fpnew_pkg:: POSINF:    vfrsqrt7_o.vfrsqrt7_e64        = 64'd0;
+          fpnew_pkg:: POSINF:    vfrsqrt7_o.vf7_e64    = 64'd0;
           fpnew_pkg:: POSSUBNORM,
           fpnew_pkg:: POSNORM:
                          begin
@@ -1957,13 +1957,13 @@ localparam logic [63:0] E64_mInf  = 64'hfff0000000000000;
                                 //      = (3*B+(~exp_i))/2
                                 vfrsqrt7_exp_o = E64_3xB +(~vfrsqrt7_exp_i);
                                      // dividing by 2
-                                vfrsqrt7_o.vfrsqrt7_e64.e = vfrsqrt7_exp_o[11:1];
+                                vfrsqrt7_o.vf7_e64.e = vfrsqrt7_exp_o[11:1];
                                 //Output significand(mantissa) can be found by
                                 //concatenating LSB of the normalized input exponent and
                                 //the six MSBs of the normalized input significand
-                                vfrsqrt7_o.vfrsqrt7_e64.m[51:45] = vfrsqrt7_lut({vfrsqrt7_exp_i [0],vfrsqrt7_i.m[51:46]});
+                                vfrsqrt7_o.vf7_e64.m[51:45] = vfrsqrt7_lut({vfrsqrt7_exp_i [0],vfrsqrt7_i.m[51:46]});
                                  //The output sign equals the input sign.
-                                vfrsqrt7_o.vfrsqrt7_e64.s = vfrsqrt7_i.s;
+                                vfrsqrt7_o.vf7_e64.s = vfrsqrt7_i.s;
                          end
          endcase
       return vfrsqrt7_o;
