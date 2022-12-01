f31188413a475a16da3d4fb72cbd20187781f39c
Chi Zhang <chizhang@student.ethz.ch>
2022-12-01 17:00:05 +0100

    [app] add include stdint.h for int32_t

--- apps/spmv/kernel/spmv.h
+++ apps/spmv/kernel/spmv.h
@@ -19,6 +19,8 @@
 #ifndef _SPMV_H
 #define _SPMV_H
 
+#include <stdint.h>
+
 void spmv_csr_idx32(int32_t N_ROW, int32_t *CSR_PROW, int32_t *CSR_INDEX,
                     double *CSR_DATA, double *IN_VEC, double *OUT_VEC);
 
