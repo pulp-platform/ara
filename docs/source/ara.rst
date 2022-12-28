d2a065f94f194b689b82c6a73a0238f7a86e2027
sharafat hussain <sharafat.hussain@10xengineers.ai>
2022-12-28 18:20:01 +0500

    [docs] :books:Documenation Hierarchy is created for readthedocs

---
+++ docs/source/ara.rst
@@ -0,0 +1,11 @@
+
+Ara
+====
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   cva6
+   vu1_0
+   
---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Ara'
+copyright = '2022, Sharafat Hussain'
+author = 'Sharafat Hussain'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
