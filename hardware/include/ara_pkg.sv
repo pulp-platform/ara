9973def296e3bef7b43fbd44f7f9e8bea49a5a53
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2024-02-29 16:58:24 +0100

    [hardware] Bump FPU

--- hardware/include/ara_pkg.sv
+++ hardware/include/ara_pkg.sv
@@ -80,9 +80,11 @@ package ara_pkg;
   localparam int unsigned LatFCompEW16    = 'd3;
   localparam int unsigned LatFCompEW8     = 'd2;
   localparam int unsigned LatFCompEW16Alt = 'd3;
+  localparam int unsigned LatFCompEW8Alt  = 'd2;
   localparam int unsigned LatFDivSqrt     = 'd3;
   localparam int unsigned LatFNonComp     = 'd1;
   localparam int unsigned LatFConv        = 'd2;
+  localparam int unsigned LatFDotp        = 'd0;
   // Define the maximum FPU latency
   localparam int unsigned LatFMax = LatFCompEW64;
 
