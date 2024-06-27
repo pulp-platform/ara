ad5c272b93852445ad100a0b8be67e7a9c2e46a2
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2024-06-27 12:53:46 +0200

    [apps] Add multi-precision matmul

---
+++ apps/dtype-matmul/kernel/bp-imatmul.h
@@ -0,0 +1,56 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Author: Matheus Cavalcante, ETH Zurich
+//         Samuel Riedel, ETH Zurich
+
+#ifndef MATMUL_H
+#define MATMUL_H
+
+#include "util.h"
+#include <stdint.h>
+#include <string.h>
+
+extern int64_t event_trigger;
+
+void bp_imatmul(int8_t *c, const int8_t *a, const int8_t *b,
+                const unsigned int m, const unsigned int n,
+                const unsigned int p);
+
+void bp_imatmul_4x4(int8_t *c, const int8_t *a, const int8_t *b,
+                    const unsigned int m, const unsigned int n,
+                    const unsigned int p);
+void bp_imatmul_vec_4x4_slice_init();
+void bp_imatmul_vec_4x4(int8_t *c, const int8_t *a, const int8_t *b,
+                        const unsigned int n, const unsigned int p);
+
+void bp_imatmul_8x8(int8_t *c, const int8_t *a, const int8_t *b,
+                    const unsigned int m, const unsigned int n,
+                    const unsigned int p);
+void bp_imatmul_vec_8x8_slice_init();
+void bp_imatmul_vec_8x8(int8_t *c, const int8_t *a, const int8_t *b,
+                        const unsigned int n, const unsigned int p);
+
+void bp_imatmul_16x16(int8_t *c, const int8_t *a, const int8_t *b,
+                      const unsigned int m, const unsigned int n,
+                      const unsigned int p);
+void bp_imatmul_vec_16x16_slice_init();
+void bp_imatmul_vec_16x16(int8_t *c, const int8_t *a, const int8_t *b,
+                          const unsigned int n, const unsigned int p);
+
+int bp_imatmul_verify(int8_t *result, int8_t *gold, size_t R, size_t C);
+
+#endif
---
+++ apps/dtype-matmul/kernel/hp-fmatmul.h
@@ -0,0 +1,59 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Author: Matheus Cavalcante, ETH Zurich
+//         Samuel Riedel, ETH Zurich
+
+#ifndef MATMUL_H
+#define MATMUL_H
+
+#include "util.h"
+#include <stdint.h>
+#include <string.h>
+
+#define THRESHOLD 0.001
+
+extern int64_t event_trigger;
+
+void hp_fmatmul(_Float16 *c, const _Float16 *a, const _Float16 *b,
+                const unsigned int m, const unsigned int n,
+                const unsigned int p);
+
+void hp_fmatmul_4x4(_Float16 *c, const _Float16 *a, const _Float16 *b,
+                    const unsigned int m, const unsigned int n,
+                    const unsigned int p);
+void hp_fmatmul_vec_4x4_slice_init();
+void hp_fmatmul_vec_4x4(_Float16 *c, const _Float16 *a, const _Float16 *b,
+                        const unsigned int n, const unsigned int p);
+
+void hp_fmatmul_8x8(_Float16 *c, const _Float16 *a, const _Float16 *b,
+                    const unsigned int m, const unsigned int n,
+                    const unsigned int p);
+void hp_fmatmul_vec_8x8_slice_init();
+void hp_fmatmul_vec_8x8(_Float16 *c, const _Float16 *a, const _Float16 *b,
+                        const unsigned int n, const unsigned int p);
+
+void hp_fmatmul_16x16(_Float16 *c, const _Float16 *a, const _Float16 *b,
+                      const unsigned int m, const unsigned int n,
+                      const unsigned int p);
+void hp_fmatmul_vec_16x16_slice_init();
+void hp_fmatmul_vec_16x16(_Float16 *c, const _Float16 *a, const _Float16 *b,
+                          const unsigned int n, const unsigned int p);
+
+int hp_fmatmul_verify(_Float16 *result, _Float16 *gold, size_t R, size_t C,
+                      _Float16 threshold);
+
+#endif
