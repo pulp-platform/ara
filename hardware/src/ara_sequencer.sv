0f7e64ff3713b32fbe4be52d34339e60e6698391
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2024-07-02 15:07:20 +0200

    [hardware] Assign default value to vfu function

--- hardware/src/ara_sequencer.sv
+++ hardware/src/ara_sequencer.sv
@@ -156,7 +156,7 @@ module ara_sequencer import ara_pkg::*; import rvv_pkg::*; import cf_math_pkg::i
   logic    pe_req_valid_d;
 
   // This function determines the VFU responsible for handling this operation.
-  function automatic vfu_e vfu(ara_op_e op);
+  function automatic vfu_e vfu(ara_op_e op = VADD);
     unique case (op) inside
       [VADD:VWREDSUM]      : vfu = VFU_Alu;
       [VMUL:VFWREDOSUM]    : vfu = VFU_MFpu;
