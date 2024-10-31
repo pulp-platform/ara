e2c4f4c05a92b45b79d4004ce60ce9990b045fb5
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2024-10-31 11:55:57 +0100

    [apps] Adapt mask logical tests to allowed tail behavior

--- apps/riscv-tests/isa/rv64uv/vmxnor.c
+++ apps/riscv-tests/isa/rv64uv/vmxnor.c
@@ -12,7 +12,7 @@ void TEST_CASE1() {
   VLOAD_8(v2, 0xCD, 0xEF);
   VLOAD_8(v3, 0x84, 0x21);
   asm volatile("vmxnor.mm v1, v2, v3");
-  VSET(2, e8, m1);
+  VSET(16, e8, m1);
   VCMP_U8(1, v1, 0xB6, 0x31);
 }
 
@@ -21,7 +21,7 @@ void TEST_CASE2() {
   VLOAD_8(v2, 0xCD, 0xEF);
   VLOAD_8(v3, 0xFF, 0xFF);
   asm volatile("vmxnor.mm v1, v2, v3");
-  VSET(2, e8, m1);
+  VSET(16, e8, m1);
   VCMP_U8(2, v1, 0xCD, 0xEF);
 }
 
@@ -30,7 +30,7 @@ void TEST_CASE3() {
   VLOAD_8(v2, 0xCD, 0xEF);
   VLOAD_8(v3, 0x00, 0x00);
   asm volatile("vmxnor.mm v1, v2, v3");
-  VSET(2, e8, m1);
+  VSET(16, e8, m1);
   VCMP_U8(3, v1, 0x32, 0x10);
 }
 
@@ -39,7 +39,7 @@ void TEST_CASE4() {
   VLOAD_8(v2, 0xCD, 0xEF);
   VLOAD_8(v3, 0x0F, 0xF0);
   asm volatile("vmxnor.mm v1, v2, v3");
-  VSET(2, e8, m1);
+  VSET(16, e8, m1);
   VCMP_U8(4, v1, 0x3D, 0xE0);
 }
 
@@ -48,10 +48,10 @@ void TEST_CASE5() {
   VLOAD_8(v1, 0xFF, 0xFF);
   VLOAD_8(v2, 0xCD, 0xEF);
   VLOAD_8(v3, 0x84, 0x21);
-  VSET(13, e8, m1);
+  VSET(16, e8, m1);
   asm volatile("vmxnor.mm v1, v2, v3");
-  VSET(2, e8, m1);
-  VCMP_U8(5, v1, 0xB6, 0xF1);
+  VSET(16, e8, m1);
+  VCMP_U8(5, v1, 0xB6, 0x31);
 }
 
 int main(void) {
