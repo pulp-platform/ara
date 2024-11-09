7d0426faba1d01a836fe7599eeb92e7ee74d5868
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2024-11-09 15:16:55 +0100

    [apps] Improve MASKU riscv-tests

--- apps/riscv-tests/isa/rv64uv/vcpop.c
+++ apps/riscv-tests/isa/rv64uv/vcpop.c
@@ -15,24 +15,90 @@ void TEST_CASE1(void) {
   VLOAD_32(v0, 5, 0, 0, 0);
   volatile uint32_t scalar = 1337;
   volatile uint32_t OUP[] = {0, 0, 0, 0};
-  __asm__ volatile("vpopc.m %[A], v2, v0.t \n"
-                   "sw %[A], (%1) \n"
-                   :
-                   : [A] "r"(scalar), "r"(OUP));
+  asm volatile("vpopc.m %[A], v2, v0.t \n"
+               "sw %[A], (%1) \n"
+               :
+               : [A] "r"(scalar), "r"(OUP));
   XCMP(1, OUP[0], 2);
+
+  VSET(32, e32, m1);
+  VLOAD_32(v8, 0xFFFFFFF7FFFFFFFF, 0x88, 0x1, 0x1F, 0xFFFFFFF7FFFFFFFF, 0x88,
+           0x1, 0x1F, 0xFFFFFFF7FFFFFFFF, 0x88, 0x1, 0x1F, 0xFFFFFFF7FFFFFFFF,
+           0x88, 0x1, 0x1F, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_32(v0, 0xffffffffffffffff, 0xfffffffffffffff7, 0xffffffffffffffff,
+           0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
+           0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
+           0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
+           0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
+           0xefffffffffffffff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  VSET(1024, e8, m8);
+  asm volatile("vpopc.m %[A], v8, v0.t \n"
+               "sw %[A], (%1) \n"
+               :
+               : [A] "r"(scalar), "r"(OUP));
+  XCMP(2, OUP[0], 159);
 }
 
 // unmasked
 void TEST_CASE2(void) {
   VSET(4, e32, m1);
-  VLOAD_32(v2, 0xF, 0, 0, 0);
+  VLOAD_32(v2, 0xFFFFFFF7FFFFFFFF, 0x88, 0x1, 0x1F);
   volatile uint32_t scalar = 1337;
   volatile uint32_t OUP[] = {0, 0, 0, 0};
-  __asm__ volatile("vpopc.m %[A], v2 \n"
-                   "sw %[A], (%1) \n"
-                   :
-                   : [A] "r"(scalar), "r"(OUP));
-  XCMP(2, OUP[0], 4);
+  VSET(128, e32, m2);
+  asm volatile("vpopc.m %[A], v2 \n"
+               "sw %[A], (%1) \n"
+               :
+               : [A] "r"(scalar), "r"(OUP));
+  XCMP(3, OUP[0], 40);
+
+  VSET(8, e32, m1);
+  VLOAD_32(v0, 0xFFFFFFF7FFFFFFFF, 0x88, 0x1, 0x1F, 0xFFFFFFF7FFFFFFFF, 0x88,
+           0x1, 0x1F);
+  VSET(256, e8, m8);
+  asm volatile("vpopc.m %[A], v0 \n"
+               "sw %[A], (%1) \n"
+               :
+               : [A] "r"(scalar), "r"(OUP));
+  XCMP(4, OUP[0], 80);
+
+  VSET(16, e32, m1);
+  VLOAD_32(v0, 0xFFFFFFF7FFFFFFFF, 0x88, 0x1, 0x1F, 0xFFFFFFF7FFFFFFFF, 0x88,
+           0x1, 0x1F, 0xFFFFFFF7FFFFFFFF, 0x88, 0x1, 0x1F, 0xFFFFFFF7FFFFFFFF,
+           0x88, 0x1, 0x1F);
+  VSET(1024, e8, m8);
+  asm volatile("vpopc.m %[A], v0 \n"
+               "sw %[A], (%1) \n"
+               :
+               : [A] "r"(scalar), "r"(OUP));
+  XCMP(5, OUP[0], 160);
+
+  VSET(8, e32, m1);
+  VLOAD_32(v2, 0xFFFFFFF7FFFFFFFF, 0x88, 0x1, 0x1F, 0xFFFFFFF7FFFFFFFF, 0x88,
+           0x1, 0x1F);
+  VSET(256, e8, m1);
+  asm volatile("vpopc.m %[A], v2 \n"
+               "sw %[A], (%1) \n"
+               :
+               : [A] "r"(scalar), "r"(OUP));
+  XCMP(6, OUP[0], 80);
+
+  VSET(2, e32, m1);
+  VLOAD_8(v2, 0xFF, 0x88);
+  VSET(16, e16, m1);
+  asm volatile("vcpop.m %[A], v2 \n"
+               "sw %[A], (%1) \n"
+               :
+               : [A] "r"(scalar), "r"(OUP));
+  XCMP(7, OUP[0], 10);
+
+  VSET(4, e32, m1);
+  VLOAD_32(v2, 0xF, 0, 0, 0);
+  asm volatile("vpopc.m %[A], v2 \n"
+               "sw %[A], (%1) \n"
+               :
+               : [A] "r"(scalar), "r"(OUP));
+  XCMP(8, OUP[0], 4);
 }
 
 int main(void) {
--- apps/riscv-tests/isa/rv64uv/vmsbf.c
+++ apps/riscv-tests/isa/rv64uv/vmsbf.c
@@ -10,17 +10,141 @@
 void TEST_CASE1() {
   VSET(16, e8, m1);
   VLOAD_8(v3, 8, 0, 0, 0, 0, 0, 0, 0);
-  __asm__ volatile("vmsbf.m v2, v3");
-  VCMP_U8(1, v2, 7, 0, 0, 0, 0, 0, 0, 0);
+  asm volatile("vmsbf.m v2, v3");
+  VSET(2, e8, m1);
+  VCMP_U8(1, v2, 7, 0);
 }
 
 void TEST_CASE2() {
   VSET(16, e8, m1);
+  VLOAD_8(v3, 4, 0, 0, 0, 0, 0, 0, 0);
+  asm volatile("vmsbf.m v2, v3");
+  VSET(2, e8, m1);
+  VCMP_U8(2, v2, 3, 0);
+}
+
+void TEST_CASE3() {
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  asm volatile("vmsbf.m v2, v3");
+  VSET(2, e8, m1);
+  VCMP_U8(3, v2, 0xff, 0xff);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  asm volatile("vmsbf.m v2, v3");
+  VSET(2, e8, m1);
+  VCMP_U8(4, v2, 0, 0);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 0x08, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  VSET(16, e32, m1);
+  asm volatile("vmsbf.m v2, v3");
+  VSET(2, e8, m1);
+  VCMP_U8(5, v2, 0x07, 0x00);
+
+  VSET(8, e64, m2);
+  VLOAD_64(v4, 0, 0, 0, 0, 0, 1, 0, 0);
+  VSET(512, e8, m2);
+  asm volatile("vmsbf.m v2, v4");
+  VSET(16, e32, m2);
+  VCMP_U32(6, v2, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
+           0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0, 0, 0,
+           0, 0, 0);
+
+  VSET(16, e64, m2);
+  VLOAD_64(v4, 0, 0, 0, 0, 0, 1, 0, 0, 1685, 0, 0, 1, 0, 0, 0, 0);
+  VSET(1024, e8, m2);
+  asm volatile("vmsbf.m v2, v4");
+  VSET(32, e32, m2);
+  VCMP_U32(7, v2, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
+           0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+}
+
+void TEST_CASE4() {
+  VSET(16, e8, m1);
   VLOAD_8(v3, 8, 0, 0, 0, 0, 0, 0, 0);
   VLOAD_8(v0, 3, 0, 0, 0, 0, 0, 0, 0);
   VCLEAR(v2);
-  __asm__ volatile("vmsbf.m v2, v3, v0.t");
-  VCMP_U8(2, v2, 3, 0, 0, 0, 0, 0, 0, 0);
+  VSET(2, e8, m1);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VCMP_U8(8, v2, 3, 0);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 8, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 5, 0, 0, 0, 0, 0, 0, 0);
+  VCLEAR(v2);
+  VSET(16, e8, m1);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VSET(2, e8, m1);
+  VCMP_U8(9, v2, 5, 0);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 0x18, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 0xf7, 0, 0, 0, 0, 0, 0, 0);
+  VCLEAR(v2);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VSET(2, e8, m1);
+  VCMP_U8(10, v2, 0x07, 0);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 0x18, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 0xef, 0, 0, 0, 0, 0, 0, 0);
+  VCLEAR(v2);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VSET(2, e8, m1);
+  VCMP_U8(11, v2, 0x07, 0);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 0x8, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 0xf7, 0xff, 0, 0, 0, 0, 0, 0);
+  VCLEAR(v2);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VSET(2, e8, m1);
+  VCMP_U8(12, v2, 0xf7, 0xff);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 0x38, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 0xf7, 0, 0, 0, 0, 0, 0, 0);
+  VCLEAR(v2);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VSET(2, e8, m1);
+  VCMP_U8(13, v2, 0x7, 0);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 8, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 5, 0, 0, 0, 0, 0, 0, 0);
+  VCLEAR(v2);
+  VSET(16, e8, m1);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VCMP_U8(14, v2, 5, 0);
+}
+
+void TEST_CASE5() {
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 8, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 11, 0, 0, 0, 0, 0, 0, 0);
+  VCLEAR(v2);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VSET(2, e8, m1);
+  VCMP_U8(15, v2, 3, 0);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 8, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 11, 0, 0, 0, 0, 0, 0, 0);
+  VCLEAR_AT_ONE(v2);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VSET(2, e8, m1);
+  VCMP_U8(16, v2, 0xf7, 0xff);
+
+  VSET(8, e8, m1);
+  VLOAD_8(v3, 0x94, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 0xC3, 0, 0, 0, 0, 0, 0, 0);
+  VCLEAR(v2);
+  asm volatile("vmsbf.m v2, v3, v0.t");
+  VSET(1, e8, m1);
+  VCMP_U8(17, v2, 0x43);
 }
 
 int main(void) {
@@ -29,5 +153,8 @@ int main(void) {
   enable_fp();
   TEST_CASE1();
   TEST_CASE2();
+  TEST_CASE3();
+  TEST_CASE4();
+  TEST_CASE5();
   EXIT_CHECK();
 }
