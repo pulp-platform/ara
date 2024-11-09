f230ca7d5597086e165977e81dbed4139944a9e2
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2024-11-09 15:16:55 +0100

    [apps] Improve MASKU riscv-tests

--- apps/riscv-tests/isa/rv64uv/vmsof.c
+++ apps/riscv-tests/isa/rv64uv/vmsof.c
@@ -10,8 +10,9 @@
 void TEST_CASE1() {
   VSET(16, e8, m1);
   VLOAD_8(v3, 8, 0, 0, 0, 0, 0, 0, 0);
-  __asm__ volatile("vmsof.m v2, v3");
-  VCMP_U8(1, v2, 8, 0, 0, 0, 0, 0, 0, 0);
+  asm volatile("vmsof.m v2, v3");
+  VSET(2, e8, m1);
+  VCMP_U8(1, v2, 8, 0);
 }
 
 void TEST_CASE2() {
@@ -19,8 +20,17 @@ void TEST_CASE2() {
   VLOAD_8(v3, 0, 0, 0, 1, 0, 0, 0, 0);
   VLOAD_8(v0, 3, 0, 0, 0, 0, 0, 0, 0);
   VCLEAR(v2);
-  __asm__ volatile("vmsof.m v2, v3, v0.t");
-  VCMP_U8(2, v2, 0, 0, 0, 0, 0, 0, 0, 0);
+  asm volatile("vmsof.m v2, v3, v0.t");
+  VSET(2, e8, m1);
+  VCMP_U8(2, v2, 0, 0);
+
+  VSET(16, e8, m1);
+  VLOAD_8(v3, 0x38, 0, 0, 0, 0, 0, 0, 0);
+  VLOAD_8(v0, 0xf7, 0, 0, 0, 0, 0, 0, 0);
+  VCLEAR(v2);
+  asm volatile("vmsof.m v2, v3, v0.t");
+  VSET(2, e8, m1);
+  VCMP_U8(3, v2, 0x10, 0);
 }
 
 int main(void) {
